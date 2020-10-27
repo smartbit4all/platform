b3d24a4353c97ec260a180f62704c246bbd809f1
Balazs Horvath <balazs.horvath@it4allhungary.onmicrosoft.com>
2020-10-27 17:11:27 +0100

    [#1443] DetailsDrawerFooter fix 2

--- ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/detailsdrawer/DetailsDrawerFooter.java
+++ ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/detailsdrawer/DetailsDrawerFooter.java
@@ -1,5 +1,6 @@
 package org.smartbit4all.ui.vaadin.components.detailsdrawer;
 
+import java.util.function.Supplier;
 import org.smartbit4all.ui.vaadin.components.FlexBoxLayout;
 import org.smartbit4all.ui.vaadin.layout.size.Horizontal;
 import org.smartbit4all.ui.vaadin.layout.size.Right;
@@ -13,46 +14,51 @@ import com.vaadin.flow.shared.Registration;
 
 public class DetailsDrawerFooter extends FlexBoxLayout {
 
-	private Button view;
-	private Button delete;
-	private Button save;
-    private Button cancel;
+  private Button view;
+  private Button delete;
+  private Button save;
+  private Button cancel;
 
-	public DetailsDrawerFooter() {
-		setBackgroundColor(LumoStyles.Color.Contrast._5);
-		setPadding(Horizontal.RESPONSIVE_L, Vertical.S);
-		setSpacing(Right.S);
-		setJustifyContentMode(JustifyContentMode.EVENLY);
-		setWidthFull();
+  public DetailsDrawerFooter() {
+    setBackgroundColor(LumoStyles.Color.Contrast._5);
+    setPadding(Horizontal.RESPONSIVE_L, Vertical.S);
+    setSpacing(Right.S);
+    setJustifyContentMode(JustifyContentMode.EVENLY);
+    setWidthFull();
+  }
 
-		view = UIUtils.createPrimaryButton(getTranslation("title.view"));
-		delete = UIUtils.createTertiaryButton(getTranslation("title.delete"));
-		save = UIUtils.createPrimaryButton(getTranslation("title.save"));
-		cancel = UIUtils.createTertiaryButton(getTranslation("title.cancel"));
-	}
+  public Registration addViewListener(
+      ComponentEventListener<ClickEvent<Button>> listener) {
+    view = addButton(view, () -> UIUtils.createPrimaryButton(getTranslation("title.view")));
+    return view.addClickListener(listener);
+  }
 
-	public Registration addViewListener(
-			ComponentEventListener<ClickEvent<Button>> listener) {
-	    add(view);
-		return view.addClickListener(listener);
-	}
+  public Registration addDeleteListener(
+      ComponentEventListener<ClickEvent<Button>> listener) {
+    delete = addButton(delete, () -> UIUtils.createTertiaryButton(getTranslation("title.delete")));
+    return delete.addClickListener(listener);
+  }
 
-	public Registration addDeleteListener(
-			ComponentEventListener<ClickEvent<Button>> listener) {
-	    add(delete);
-		return delete.addClickListener(listener);
-	}
-	
-	public Registration addSaveListener(
-	    ComponentEventListener<ClickEvent<Button>> listener) {
-	  add(save);
-	  return save.addClickListener(listener);
-	}
-	
-	public Registration addCancelListener(
-	    ComponentEventListener<ClickEvent<Button>> listener) {
-	  add(cancel);
-	  return cancel.addClickListener(listener);
-	}
+  public Registration addSaveListener(
+      ComponentEventListener<ClickEvent<Button>> listener) {
+    save = addButton(save, () -> UIUtils.createPrimaryButton(getTranslation("title.save")));
+    return save.addClickListener(listener);
+  }
 
+  public Registration addCancelListener(
+      ComponentEventListener<ClickEvent<Button>> listener) {
+
+    cancel = addButton(cancel, () -> UIUtils.createTertiaryButton(getTranslation("title.cancel")));
+    return cancel.addClickListener(listener);
+  }
+
+  private Button addButton(Button buttonField, Supplier<Button> buttonFactory) {
+    if(buttonField != null) {
+      remove(buttonField);
+    }
+    buttonField = buttonFactory.get();
+    add(buttonField);
+    return buttonField;
+  }
+  
 }
