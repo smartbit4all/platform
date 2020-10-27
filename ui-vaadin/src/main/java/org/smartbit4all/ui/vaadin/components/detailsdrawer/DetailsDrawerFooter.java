36f1168c652aa562b911be496929bcddafdd9f3c
Balazs Horvath <balazs.horvath@it4allhungary.onmicrosoft.com>
2020-10-27 16:50:52 +0100

    [#1443] DetailsDrawerFooter fix

--- ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/detailsdrawer/DetailsDrawerFooter.java
+++ ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/detailsdrawer/DetailsDrawerFooter.java
@@ -1,21 +1,22 @@
 package org.smartbit4all.ui.vaadin.components.detailsdrawer;
 
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.ComponentEventListener;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.shared.Registration;
-
 import org.smartbit4all.ui.vaadin.components.FlexBoxLayout;
 import org.smartbit4all.ui.vaadin.layout.size.Horizontal;
 import org.smartbit4all.ui.vaadin.layout.size.Right;
 import org.smartbit4all.ui.vaadin.layout.size.Vertical;
 import org.smartbit4all.ui.vaadin.util.LumoStyles;
 import org.smartbit4all.ui.vaadin.util.UIUtils;
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.shared.Registration;
 
 public class DetailsDrawerFooter extends FlexBoxLayout {
 
 	private Button view;
 	private Button delete;
+	private Button save;
+    private Button cancel;
 
 	public DetailsDrawerFooter() {
 		setBackgroundColor(LumoStyles.Color.Contrast._5);
@@ -26,17 +27,32 @@ public class DetailsDrawerFooter extends FlexBoxLayout {
 
 		view = UIUtils.createPrimaryButton(getTranslation("title.view"));
 		delete = UIUtils.createTertiaryButton(getTranslation("title.delete"));
-		add(view, delete);
+		save = UIUtils.createPrimaryButton(getTranslation("title.save"));
+		cancel = UIUtils.createTertiaryButton(getTranslation("title.cancel"));
 	}
 
 	public Registration addViewListener(
 			ComponentEventListener<ClickEvent<Button>> listener) {
+	    add(view);
 		return view.addClickListener(listener);
 	}
 
 	public Registration addDeleteListener(
 			ComponentEventListener<ClickEvent<Button>> listener) {
+	    add(delete);
 		return delete.addClickListener(listener);
+	}
+	
+	public Registration addSaveListener(
+	    ComponentEventListener<ClickEvent<Button>> listener) {
+	  add(save);
+	  return save.addClickListener(listener);
+	}
+	
+	public Registration addCancelListener(
+	    ComponentEventListener<ClickEvent<Button>> listener) {
+	  add(cancel);
+	  return cancel.addClickListener(listener);
 	}
 
 }
