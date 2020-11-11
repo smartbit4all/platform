7001cc17534f61bccdb1c86782a40228c9c10924
Balazs Horvath <balazs.horvath@it4allhungary.onmicrosoft.com>
2020-11-11 09:25:49 +0100

    [#1658] DynamicFilter skeleton impl - add ui impl missing methods

--- ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/filter/DynamicFilterViewUI.java
+++ ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/filter/DynamicFilterViewUI.java
@@ -1,5 +1,10 @@
 package org.smartbit4all.ui.vaadin.components.filter;
 
+import java.util.List;
+import org.smartbit4all.api.dynamicfilter.bean.DynamicFilter;
+import org.smartbit4all.api.dynamicfilter.bean.DynamicFilterDescriptor;
+import org.smartbit4all.api.dynamicfilter.bean.DynamicFilterGroup;
+import org.smartbit4all.api.dynamicfilter.bean.DynamicFilterOption;
 import org.smartbit4all.ui.common.filter.DynamicFilterController;
 import org.smartbit4all.ui.common.filter.DynamicFilterView;
 
@@ -14,6 +19,32 @@ public class DynamicFilterViewUI implements DynamicFilterView {
   }
 
   private void init() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void renderFilterConfig(List<DynamicFilterDescriptor> filterConfig) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void renderFilter(String groupId, String filterId, DynamicFilter dynamicFilter,
+      List<DynamicFilterOption> filterOptions) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void renderFilter(String filterId, DynamicFilter dynamicFilter) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void renderGroup(String parentGroupId, String childGroupId,
+      DynamicFilterGroup childGroup) {
     // TODO Auto-generated method stub
     
   }
