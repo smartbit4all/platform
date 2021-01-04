0e705cd1b8f55dab8aff1d3af39ddf98b413e207
Balazs Horvath <balazs.horvath@it4allhungary.onmicrosoft.com>
2021-01-04 09:23:11 +0100

    [#2066] navigation service definitions in yaml

---
+++ api/src-gen/main/java/org/smartbit4all/api/navigation/bean/NavigationResponse.java
@@ -0,0 +1,114 @@
+package org.smartbit4all.api.navigation.bean;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.validation.Valid;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * NavigationResponse
+ */
+
+public class NavigationResponse   {
+  @JsonProperty("associationMetaUri")
+  private URI associationMetaUri;
+
+  @JsonProperty("referenceEntries")
+  @Valid
+  private List<NavigationReferenceEntry> referenceEntries = null;
+
+  public NavigationResponse associationMetaUri(URI associationMetaUri) {
+    this.associationMetaUri = associationMetaUri;
+    return this;
+  }
+
+  /**
+   * Get associationMetaUri
+   * @return associationMetaUri
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public URI getAssociationMetaUri() {
+    return associationMetaUri;
+  }
+
+  public void setAssociationMetaUri(URI associationMetaUri) {
+    this.associationMetaUri = associationMetaUri;
+  }
+
+  public NavigationResponse referenceEntries(List<NavigationReferenceEntry> referenceEntries) {
+    this.referenceEntries = referenceEntries;
+    return this;
+  }
+
+  public NavigationResponse addReferenceEntriesItem(NavigationReferenceEntry referenceEntriesItem) {
+    if (this.referenceEntries == null) {
+      this.referenceEntries = new ArrayList<>();
+    }
+    this.referenceEntries.add(referenceEntriesItem);
+    return this;
+  }
+
+  /**
+   * Get referenceEntries
+   * @return referenceEntries
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public List<NavigationReferenceEntry> getReferenceEntries() {
+    return referenceEntries;
+  }
+
+  public void setReferenceEntries(List<NavigationReferenceEntry> referenceEntries) {
+    this.referenceEntries = referenceEntries;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    NavigationResponse navigationResponse = (NavigationResponse) o;
+    return Objects.equals(this.associationMetaUri, navigationResponse.associationMetaUri) &&
+        Objects.equals(this.referenceEntries, navigationResponse.referenceEntries);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(associationMetaUri, referenceEntries);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class NavigationResponse {\n");
+    
+    sb.append("    associationMetaUri: ").append(toIndentedString(associationMetaUri)).append("\n");
+    sb.append("    referenceEntries: ").append(toIndentedString(referenceEntries)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
