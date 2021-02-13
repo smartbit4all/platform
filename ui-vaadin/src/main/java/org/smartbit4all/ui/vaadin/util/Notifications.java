ee21d026f16e107661373757941103148b647f14
Balazs Horvath <balazs.horvath@it4allhungary.onmicrosoft.com>
2021-02-13 11:45:17 +0100

    [#2514] make vaadin notifications configurable

--- ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/util/Notifications.java
+++ ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/util/Notifications.java
@@ -15,17 +15,53 @@
 package org.smartbit4all.ui.vaadin.util;
 
 import org.smartbit4all.ui.vaadin.util.Css.TextColor;
+import org.springframework.stereotype.Component;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.Notification.Position;
 
+@Component
 public class Notifications {
+  
+  private static Notifications INSTANCE;
+  
+  private NotificationHandler notificationHandler;
+  
+  /**
+   * <b>CAUTION!</b><br>
+   * <b>Notifications can be initialized only by the Spring framework!</b><br>
+   * Initializing in code causes {@link IllegalStateException}!
+   */
+  public Notifications(NotificationHandler notificationHandler) {
+    if (INSTANCE != null) {
+      throw new IllegalStateException(this.getClass()
+          + " has been already initialized by the Spring framework! Use the static methods instead!");
+    }
+    this.notificationHandler = notificationHandler;
+    INSTANCE = this;
+  }
 
-  public static void showNotification(String text) {
-    Notification.show(text, 3000, Notification.Position.BOTTOM_CENTER);
+  public static void showNotification(String message) {
+    if(INSTANCE != null) {
+      INSTANCE.notificationHandler.showNotification(message);
+    } else {
+      ShowDefaultNotification(message);
+    }
   }
 
   public static void showErrorNotification(String message) {
+    if(INSTANCE != null) {
+      INSTANCE.notificationHandler.showErrorNotification(message);
+    } else {
+      showDefaultErrorNotification(message);
+    }
+  }
+
+  private static void ShowDefaultNotification(String message) {
+    Notification.show(message, 3000, Notification.Position.BOTTOM_CENTER);
+  }
+
+  private static void showDefaultErrorNotification(String message) {
     Label content = new Label(message);
     Css.setTextColor(TextColor.ERROR, content);
     Notification notification = new Notification(content);
@@ -34,4 +70,11 @@ public class Notifications {
     notification.open();
   }
 
+  public static interface NotificationHandler {
+    
+    void showNotification(String message);
+
+    void showErrorNotification(String message);
+  }
+  
 }
