64a2d0069cc62b9ee850950a63f63e0c1a639262
Zoltan Suller <zoltan.suller@it4all.hu>
2021-02-19 05:03:11 +0100

    [#2566] ObjectEditingImpl: remove unused annotations

--- api/src/main/java/org/smartbit4all/api/object/ObjectEditingImpl.java
+++ api/src/main/java/org/smartbit4all/api/object/ObjectEditingImpl.java
@@ -6,19 +6,16 @@ public class ObjectEditingImpl implements ObjectEditing{
 
   protected ObjectPublisherImpl publisher;
 
-  @NotifyListeners
   @Override
   public void setValue(String propertyPath, Object value) {
     ref.setValueByPath(propertyPath, value);
   }
 
-  @NotifyListeners
   @Override
   public void addValue(String collectionPath, Object value) {
     ref.addValueByPath(collectionPath, value);
   }
 
-  @NotifyListeners
   @Override
   public void removeValue(String collectionElementPath) {
     ref.removeValueByPath(collectionElementPath);
