ead8c402403104948707c5a04d9b4a42ce80da05
norbertsalamon <norbert.salamon@it4all.hu>
2021-02-22 17:07:40 +0100

    [#2704] Screen level VirtualKeyboard

--- ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/VirtualKeyboard.java
+++ ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/VirtualKeyboard.java
@@ -7,8 +7,10 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -18,29 +20,34 @@ import com.vaadin.flow.component.textfield.TextField;
 public class VirtualKeyboard extends Composite<Dialog> {
 
   private static final String CSSCLASS = "virtual-keyboard";
-  
+
   private FlexLayout wrapper;
-  
+
   private TextField workingTextField;
-  
+  private TextField selectedTextField;
+  private ComboBox<TextField> cbxTextFieldsOnLayout;
+
   private List<String>[] characters;
-  
+
   public VirtualKeyboard(List<String>... characters) {
     this.characters = characters;
     init();
   }
-  
+
   protected void init() {
     wrapper = new FlexLayout();
     addClassNameToComponent(wrapper, "virtual-keyboard-wrapper");
     workingTextField = new TextField();
     addClassNameToComponent(workingTextField, "virtual-keyboard-working-textfield");
+    cbxTextFieldsOnLayout = new ComboBox<>();
+
     Component header = createHeader();
     FlexLayout contentWrapper = createContentWrapper();
     FlexLayout buttonWrapper = createButtonWrapper();
-    
+
     wrapper.add(header, contentWrapper, buttonWrapper);
     getContent().add(wrapper);
+    getContent().addDialogCloseActionListener(closeAction -> close());
   }
 
   protected Component createHeader() {
@@ -48,32 +55,33 @@ public class VirtualKeyboard extends Composite<Dialog> {
     header.add(workingTextField);
     return header;
   }
-  
+
   protected FlexLayout createContentWrapper() {
     FlexLayout contentWrapper = new FlexLayout();
     for (List<String> characterList : characters) {
       VerticalLayout characterListLayout = new VerticalLayout();
       for (String character : characterList) {
         Button characterButton = new Button(character);
-        characterButton.addClickListener(click -> workingTextField.setValue(workingTextField.getValue() + character));
+        characterButton.addClickListener(
+            click -> workingTextField.setValue(workingTextField.getValue() + character));
         characterListLayout.add(characterButton);
       }
       contentWrapper.add(characterListLayout);
     }
     return contentWrapper;
   }
-  
+
   protected FlexLayout createButtonWrapper() {
     FlexLayout buttonWrapper = new FlexLayout();
-    
-    Button okButton = new Button("OK");
-    okButton.addClickListener(click -> save());
+
+    Button saveButton = new Button("Mentés");
+    saveButton.addClickListener(click -> save());
     Button cancelButton = new Button("Mégse");
     cancelButton.addClickListener(click -> close());
-    
+
     HorizontalLayout buttonLayout = new HorizontalLayout();
     addClassNameToComponent(buttonLayout, "virtual-keyboard-buttonlayout");
-    buttonLayout.add(okButton, cancelButton);
+    buttonLayout.add(saveButton, cancelButton);
     buttonWrapper.add(buttonLayout);
     return buttonWrapper;
   }
@@ -81,29 +89,60 @@ public class VirtualKeyboard extends Composite<Dialog> {
   public void open() {
     getContent().open();
   }
-  
+
   public void close() {
+    workingTextField.setValue("");
     getContent().close();
   }
-  
-  public void setTextValue(String textValue) {
-    workingTextField.setValue(textValue);
-  }
-  
+
   List<Consumer<String>> onSaveMethods = new ArrayList<>();
-  
+
   public void addOnSaveMethod(Consumer<String> onSaveConsumer) {
     onSaveMethods.add(onSaveConsumer);
   }
-  
+
   private void save() {
-    onSaveMethods.forEach(m -> m.accept(workingTextField.getValue()));
-    this.close();
+    Dialog dialog = new Dialog();
+    Label selectLabel =
+        new Label("Kérem válassza ki a kívánt mezőt, amibe kerüljön a megadott szöveg!");
+    Button saveButton = new Button("Mentés");
+    Button cancelButton = new Button("Mégse");
+
+    FlexLayout savingDialogLayout = new FlexLayout();
+    addClassNameToComponent(savingDialogLayout, "virtual-keyboard-savingdialog-layout");
+    HorizontalLayout buttonLayout = new HorizontalLayout();
+    buttonLayout.add(saveButton, cancelButton);
+    savingDialogLayout.add(selectLabel, cbxTextFieldsOnLayout, buttonLayout);
+    dialog.add(savingDialogLayout);
+
+    saveButton.addClickListener(save -> {
+      onSaveMethods.forEach(m -> m.accept(workingTextField.getValue()));
+      cbxTextFieldsOnLayout.setValue(null);
+      dialog.close();
+    });
+    cancelButton.addClickListener(cancel -> {
+      cbxTextFieldsOnLayout.setValue(null);
+      dialog.close();
+    });
+    dialog.open();
   }
-  
+
   private void addClassNameToComponent(HasStyle hasStyle, String className) {
     hasStyle.addClassName(CSSCLASS);
     hasStyle.addClassName(className);
   }
-  
+
+  public void setTextFieldsOnLayout(List<TextField> textFieldsOnLayout) {
+    cbxTextFieldsOnLayout.setItems(textFieldsOnLayout);
+    cbxTextFieldsOnLayout.setItemLabelGenerator(TextField::getLabel);
+    cbxTextFieldsOnLayout.addValueChangeListener(select -> setSelectedTextField(select.getValue()));
+  }
+
+  public TextField getSelectedTextField() {
+    return selectedTextField;
+  }
+
+  public void setSelectedTextField(TextField selectedTextField) {
+    this.selectedTextField = selectedTextField;
+  }
 }
--- ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/VirtualKeyboardButton.java
+++ ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/VirtualKeyboardButton.java
@@ -1,18 +1,23 @@
 package org.smartbit4all.ui.vaadin.components;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.textfield.TextField;
 
 public class VirtualKeyboardButton extends Composite<Button> {
   
   private static final String CSSCLASS = "virtual-keyboard-button";
-  private TextField workingTextField;
+  private FlexLayout layout;
   private VirtualKeyboard virtualKeyboard;
+  private List<TextField> textFieldsOnLayout;
   
-  public VirtualKeyboardButton(VirtualKeyboard virtualKeyboard, TextField workingTextField) {
+  public VirtualKeyboardButton(VirtualKeyboard virtualKeyboard, FlexLayout layout) {
     this.virtualKeyboard = virtualKeyboard;
-    this.workingTextField = workingTextField;
+    this.layout = layout;
     init();
   }
   
@@ -26,9 +31,21 @@ public class VirtualKeyboardButton extends Composite<Button> {
   }
 
   public void clickListener() {
-    virtualKeyboard.setTextValue(workingTextField.getValue());
-    virtualKeyboard.addOnSaveMethod(value -> workingTextField.setValue(value));
+    textFieldsOnLayout = getTextFieldsOnlayout();
+    virtualKeyboard.setTextFieldsOnLayout(textFieldsOnLayout);
+    virtualKeyboard.addOnSaveMethod(value -> virtualKeyboard.getSelectedTextField().setValue(value));
     virtualKeyboard.open();
+  }
+  
+  protected List<TextField> getTextFieldsOnlayout() {
+    ArrayList<TextField> textFieldsOnLayout = new ArrayList<>();
+    for (int i = 0; i < layout.getComponentCount(); i++) {
+      Component component = layout.getComponentAt(i);
+      if (component instanceof TextField) {
+        textFieldsOnLayout.add((TextField)component);
+      }
+    }
+    return textFieldsOnLayout;
   }
 
 }
