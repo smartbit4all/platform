cf034f32adc48e80c460f0f04257759402175fe4
norbertsalamon <norbert.salamon@it4all.hu>
2021-02-24 07:27:11 +0100

    [#2703] Recursive search on actual layout

--- ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/VirtualKeyboardButton.java
+++ ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/VirtualKeyboardButton.java
@@ -4,23 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.HasOrderedComponents;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.textfield.TextField;
 
 public class VirtualKeyboardButton extends Composite<Button> {
-  
+
   private static final String CSSCLASS = "virtual-keyboard-button";
-  private FlexLayout layout;
+  private HasOrderedComponents<FlexLayout> layout;
   private VirtualKeyboard virtualKeyboard;
   private List<TextField> textFieldsOnLayout;
-  
-  public VirtualKeyboardButton(VirtualKeyboard virtualKeyboard, FlexLayout layout) {
+
+  public VirtualKeyboardButton(VirtualKeyboard virtualKeyboard, HasOrderedComponents<FlexLayout> layout) {
     this.virtualKeyboard = virtualKeyboard;
     this.layout = layout;
     init();
   }
-  
+
   protected void init() {
     addButtonName("Virtual");
     getContent().addClickListener(click -> clickListener());
@@ -31,18 +32,21 @@ public class VirtualKeyboardButton extends Composite<Button> {
   }
 
   public void clickListener() {
-    textFieldsOnLayout = getTextFieldsOnlayout();
+    textFieldsOnLayout = getTextFieldsOnlayout(layout);
     virtualKeyboard.setTextFieldsOnLayout(textFieldsOnLayout);
-    virtualKeyboard.addOnSaveMethod(value -> virtualKeyboard.getSelectedTextField().setValue(value));
+    virtualKeyboard
+        .addOnSaveMethod(value -> virtualKeyboard.getSelectedTextField().setValue(value));
     virtualKeyboard.open();
   }
   
-  protected List<TextField> getTextFieldsOnlayout() {
+  protected List<TextField> getTextFieldsOnlayout(HasOrderedComponents<FlexLayout> layout) {
     ArrayList<TextField> textFieldsOnLayout = new ArrayList<>();
     for (int i = 0; i < layout.getComponentCount(); i++) {
       Component component = layout.getComponentAt(i);
       if (component instanceof TextField) {
-        textFieldsOnLayout.add((TextField)component);
+        textFieldsOnLayout.add((TextField) component);
+      } else if (component instanceof HasOrderedComponents<?>) {
+        textFieldsOnLayout.addAll(getTextFieldsOnlayout((HasOrderedComponents<FlexLayout>)component));
       }
     }
     return textFieldsOnLayout;
