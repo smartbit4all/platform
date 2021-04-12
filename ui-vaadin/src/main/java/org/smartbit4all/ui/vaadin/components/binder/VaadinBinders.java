0ff562af8fe6ef06313da479041a1f01ef4d1a52
Attila Mate <attila.mate@it4all.hu>
2021-04-12 16:48:38 +0200

    [#3282] more binders

--- ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/binder/VaadinBinders.java
+++ ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/binder/VaadinBinders.java
@@ -1,17 +1,23 @@
 package org.smartbit4all.ui.vaadin.components.binder;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import org.smartbit4all.api.object.ObjectEditing;
 import com.vaadin.flow.component.HasText;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.datetimepicker.DateTimePicker;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.textfield.BigDecimalField;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.timepicker.TimePicker;
 
 public class VaadinBinders {
 
@@ -42,15 +48,31 @@ public class VaadinBinders {
     return new VaadinHasValueBinder<>(field, editing, path, BigDecimal.class);
   }
 
-  public static void bind(HasText label, ObjectEditing editing,
+  public static VaadinHasValueBinder<LocalDate> bind(DatePicker field, ObjectEditing editing,
       String path) {
-    new VaadinHasTextBinder(label, editing, path);
+    return new VaadinHasValueBinder<>(field, editing, path, LocalDate.class);
+  }
+
+  public static VaadinHasValueBinder<LocalDateTime> bind(DateTimePicker field,
+      ObjectEditing editing,
+      String path) {
+    return new VaadinHasValueBinder<>(field, editing, path, LocalDateTime.class);
+  }
+
+  public static VaadinHasValueBinder<LocalTime> bind(TimePicker field, ObjectEditing editing,
+      String path) {
+    return new VaadinHasValueBinder<>(field, editing, path, LocalTime.class);
   }
 
   public static <T> VaadinHasValueBinder<T> bind(ComboBox<T> comboBox,
       ObjectEditing editing,
       String path, Class<T> valueClass) {
     return new VaadinHasValueBinder<>(comboBox, editing, path, valueClass);
+  }
+
+  public static void bind(HasText label, ObjectEditing editing,
+      String path) {
+    new VaadinHasTextBinder(label, editing, path);
   }
 
   public static <T, E extends ObjectEditing> VaadinGridBinder<T, E> bind(Grid<T> grid, E editing,
