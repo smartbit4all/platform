72ec1640af2ffea064bb295bffe5b621d2f50e8e
Zoltán Szegedi <zoltan.szegedi@it4all.hu>
2021-05-14 10:21:57 +0200

    [#3492] Observable object path match ignore case

--- core/src/main/java/org/smartbit4all/core/object/ObservableObjectHelper.java
+++ core/src/main/java/org/smartbit4all/core/object/ObservableObjectHelper.java
@@ -8,11 +8,23 @@ public class ObservableObjectHelper {
   private ObservableObjectHelper() {}
 
   static boolean pathMatch(ChangeItem changeItem, String path, String name) {
-    if (Strings.isNullOrEmpty(path) && Strings.isNullOrEmpty(changeItem.getPath())) {
+    String changeItemNameUpper = upper(changeItem.getName());
+    String changeItemPathUpper = upper(changeItem.getPath());
+    
+    String nameUpper = upper(name);
+    String pathUpper = upper(path);
+    
+    if (Strings.isNullOrEmpty(pathUpper) && Strings.isNullOrEmpty(changeItemPathUpper)) {
       // empty paths are considered equal, check only name
-      return Objects.equals(changeItem.getName(), name);
+      return Objects.equals(changeItemNameUpper, nameUpper);
     }
-    return Objects.equals(changeItem.getPath(), path)
-        && Objects.equals(changeItem.getName(), name);
+    
+    return Objects.equals(changeItemPathUpper, pathUpper)
+        && Objects.equals(changeItemNameUpper, nameUpper);
   }
+  
+  private static String upper(String text) {
+    return text == null ? null : text.toUpperCase();
+  }
+  
 }
