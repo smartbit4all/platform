049171cfd27b807c851514e1ab2c1094012fc364
Zoltán Szegedi <zoltan.szegedi@it4all.hu>
2021-05-18 16:36:05 +0200

    [#855] StatefulApi advisor registration - fix auto proxy error

--- core/src/main/java/org/smartbit4all/core/object/proxy/ProxyStatefulApiConfiguration.java
+++ core/src/main/java/org/smartbit4all/core/object/proxy/ProxyStatefulApiConfiguration.java
@@ -1,46 +1,53 @@
 package org.smartbit4all.core.object.proxy;
 
-import org.springframework.aop.framework.autoproxy.InfrastructureAdvisorAutoProxyCreator;
+import org.springframework.aop.config.AopConfigUtils;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Role;
 
 @Configuration
-public class ProxyStatefulApiConfiguration {
+public class ProxyStatefulApiConfiguration implements BeanDefinitionRegistryPostProcessor {
 
-    /**
-    * Define Faces
-    * Be sure to specify the @Role comment here
-    *
-    * @return
-    */
-    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    @Bean
-    public BeanFactoryStatefulApiAdvisor beanFactoryStatefulApiAdvisor() {
-      BeanFactoryStatefulApiAdvisor advisor = new BeanFactoryStatefulApiAdvisor();
-        advisor.setAdvice(statefulApiInvocationHandler());
-        return advisor;
-    }
+  /**
+   * Define Faces Be sure to specify the @Role comment here
+   *
+   * @return
+   */
+  @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+  @Bean
+  public BeanFactoryStatefulApiAdvisor beanFactoryStatefulApiAdvisor() {
+    BeanFactoryStatefulApiAdvisor advisor = new BeanFactoryStatefulApiAdvisor();
 
-    /**
-    * Define Notifications
-    *
-    * @return
-    */
-    @Bean
-    public StatefulApiInterceptor statefulApiInvocationHandler() {
-        return new StatefulApiInterceptor();
-    }
+    advisor.setAdvice(statefulApiInvocationHandler());
 
-    /**
-    * Be sure to declare Infrastructure AdvisorAutoProxyCreator for post-processing of bean s
-    *
-    * @return
-    */
-    @Bean
-    public InfrastructureAdvisorAutoProxyCreator infrastructureAdvisorAutoProxyCreator() {
-        return new InfrastructureAdvisorAutoProxyCreator();
-    }
+    return advisor;
+  }
+
+  /**
+   * Define Notifications
+   *
+   * @return
+   */
+  @Bean
+  public StatefulApiInterceptor statefulApiInvocationHandler() {
+    return new StatefulApiInterceptor();
+  }
+
+  @Override
+  public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+      throws BeansException {}
+
+  @Override
+  public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry)
+      throws BeansException {
+    
+    // Ensure an auto-proxy creator is registered.
+    AopConfigUtils.registerAutoProxyCreatorIfNecessary(registry);
+  }
+
 }
