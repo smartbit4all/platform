ba26577a1787cf438b557b1a79ece7a5e26449fc
Peter Boros <peter.boros@it4all.hu>
2021-08-05 09:40:56 +0200

    [#3618] MimeTypeHandler - some documentation...

--- api/src/main/java/org/smartbit4all/api/mimetype/MimeTypeApi.java
+++ api/src/main/java/org/smartbit4all/api/mimetype/MimeTypeApi.java
@@ -6,27 +6,37 @@ import java.util.Map;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * Collects the {@link MimeTypeHandler}s available in the current application. They can be used to
+ * process a given content: extract the pages or the text of the document an so on.
+ * 
+ * @author András Palló
+ *
+ */
 public class MimeTypeApi implements InitializingBean {
 
   private Map<String, MimeTypeHandler> handlersByMimeType = new HashMap<>();
 
+  /**
+   * The available handlers autowired by the Spring. If we include a new functional module adding
+   * new handler they will be available immediately.
+   */
   @Autowired
   private List<MimeTypeHandler> handlers;
-  
-  public MimeTypeApi() {
-  }
+
+  public MimeTypeApi() {}
 
   @Override
   public void afterPropertiesSet() throws Exception {
     if (handlers != null) {
       for (MimeTypeHandler handler : handlers) {
-        for(String mimeType : handler.getAcceptedMimeTypes()) {
+        for (String mimeType : handler.getAcceptedMimeTypes()) {
           handlersByMimeType.put(mimeType, handler);
         }
       }
     }
   }
-  
+
   public MimeTypeHandler getHandler(String mimeType) {
     return handlersByMimeType.get(mimeType);
   }
--- api/src/main/java/org/smartbit4all/api/mimetype/MimeTypeHandler.java
+++ api/src/main/java/org/smartbit4all/api/mimetype/MimeTypeHandler.java
@@ -5,14 +5,20 @@ import java.util.List;
 import org.smartbit4all.api.documentview.bean.DisplayMode;
 import org.smartbit4all.types.binarydata.BinaryData;
 
+/**
+ * A handler is special API for manipulating the given kind of documents. We can extract data from
+ * this.
+ * 
+ * @author András Palló
+ */
 public interface MimeTypeHandler {
-  
+
   List<String> getAcceptedMimeTypes();
-  
+
   DisplayMode getDisplayMode();
-  
+
   List<BinaryData> getImages(BinaryData document, URI uri);
-  
+
   String getText(BinaryData document);
-  
+
 }
