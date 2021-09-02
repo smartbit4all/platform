f32d784831f3b9b6e8b4e116b802408a15e02694
zslipcsei <zsolt.lipcsei@it4all.hu>
2021-09-02 16:14:49 +0200

    [#4554] add ContentAccess api definiton + generate out the api

---
+++ content-access-rest/src-gen/main/java/org/smartbit4all/api/contentaccess/contentaccess/rest/ContentAccessApiController.java
@@ -0,0 +1,22 @@
+package org.smartbit4all.api.contentaccess.contentaccess.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+@Controller
+@RequestMapping("${openapi.contentAccess.base-path:/}")
+public class ContentAccessApiController implements ContentAccessApi {
+
+    private final ContentAccessApiDelegate delegate;
+
+    public ContentAccessApiController(@org.springframework.beans.factory.annotation.Autowired(required = false) ContentAccessApiDelegate delegate) {
+        this.delegate = Optional.ofNullable(delegate).orElse(new ContentAccessApiDelegate() {});
+    }
+
+    @Override
+    public ContentAccessApiDelegate getDelegate() {
+        return delegate;
+    }
+
+}
