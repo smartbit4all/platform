424adbbc6d343a58787f4c92f0f1175881abfdb6
Zoltán Szegedi <zoltan.szegedi@it4all.hu>
2021-09-16 09:46:48 +0200

    [#4536] Fancy Vaadin icon button

---
+++ ui-vaadin/src/main/java/org/smartbit4all/ui/vaadin/components/button/IconButton.java
@@ -0,0 +1,30 @@
+package org.smartbit4all.ui.vaadin.components.button;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+
+public class IconButton {
+
+  public static Button createIconButton(
+      String text,
+      Icon icon,
+      String iconSize,
+      ComponentEventListener<ClickEvent<Button>> listener) {
+
+    Button newButton = new Button();
+
+    icon.setSize(iconSize);
+
+    newButton.getElement().appendChild(icon.getElement());
+    newButton.getElement().appendChild(new Div(new Label(text)).getElement());
+
+    newButton.addClickListener(listener);
+
+    return newButton;
+  }
+
+}
