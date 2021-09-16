b2ee98ab60721087357983f2ce676c05b863b4f3
Palló András <andras.pallo@it4all.hu>
2021-09-16 17:14:45 +0200

    [#4734] ObjectPropertyChangeTracker can be used for ReferencedObjectChange

--- core/src/main/java/org/smartbit4all/core/object/utility/ObjectPropertyChangeTracker.java
+++ core/src/main/java/org/smartbit4all/core/object/utility/ObjectPropertyChangeTracker.java
@@ -1,7 +1,10 @@
 package org.smartbit4all.core.object.utility;
 
+import org.smartbit4all.core.object.ApiObjectRef;
 import org.smartbit4all.core.object.ObservableObject;
-import org.smartbit4all.core.object.PropertyChange;
+import org.smartbit4all.core.object.ObservableObjectImpl;
+import org.smartbit4all.core.object.PropertyEntry;
+import org.smartbit4all.core.object.PropertyMeta.PropertyKind;
 
 /**
  * Track the property value of an ObservableObject property.
@@ -16,16 +19,41 @@ public class ObjectPropertyChangeTracker<T> {
 
   public ObjectPropertyChangeTracker(ObservableObject observableObject, String propertyName) {
     property = null;
-    
-    observableObject.onPropertyChange(
-        null,
-        propertyName,
-        change -> onPropertyChange(change));
+
+    onPropertyChange(observableObject, propertyName);
+  }
+
+  private void onPropertyChange(ObservableObject observableObject, String propertyName) {
+    ApiObjectRef ref = ((ObservableObjectImpl) observableObject).getRef();
+    for (PropertyEntry entry : ref.getProperties()) {
+      if (upper(entry.getPath()).equals(upper(propertyName))) {
+        PropertyKind kind = entry.getMeta().getKind();
+        
+        if (kind.equals(PropertyKind.VALUE)) {
+          
+          observableObject.onPropertyChange(
+              null, 
+              propertyName, 
+              change -> setProperty(change.getNewValue()));
+          
+        } else if (kind.equals(PropertyKind.REFERENCE)) {
+          
+          observableObject.onReferencedObjectChange(
+              null, 
+              propertyName, 
+              change -> setProperty(change.getChange().getObject()));
+          
+        }
+      }
+    }
+  }
+  
+  private String upper(String text) {
+    return text == null ? "" : text.toUpperCase();
   }
 
   @SuppressWarnings("unchecked")
-  private void onPropertyChange(PropertyChange propertyChange) {
-    Object object = propertyChange.getNewValue();
+  private void setProperty(Object object) {
     if (object == null) {
       property = null;
     } else {
