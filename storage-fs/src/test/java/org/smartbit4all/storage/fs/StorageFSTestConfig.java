c1d0986d914a1679330108dd726159d44eecba2b
Peter Boros <peter.boros@it4all.hu>
2021-12-03 11:04:12 +0100

    [#5647] StorageFS - transaction mgmt - Spring transaction manager

--- storage-fs/src/test/java/org/smartbit4all/storage/fs/StorageFSTestConfig.java
+++ storage-fs/src/test/java/org/smartbit4all/storage/fs/StorageFSTestConfig.java
@@ -7,9 +7,11 @@ import org.smartbit4all.domain.data.storage.Storage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @Import({DomainConfig.class})
+@EnableTransactionManagement
 public class StorageFSTestConfig {
 
   public static final String TESTSCHEME = "testscheme";
@@ -24,6 +26,16 @@ public class StorageFSTestConfig {
   @Bean
   public Storage testStorageScheme(ObjectApi objectApi, StorageFS storageFS) {
     return new Storage(TESTSCHEME, objectApi, storageFS);
+  }
+
+  @Bean
+  public StorageFSTestApi storageFSTestApi() {
+    return new StorageFSTestApiImpl();
+  }
+
+  @Bean("storageTx")
+  public StorageTransactionManager transactionManager() {
+    return new StorageTransactionManager();
   }
 
 }
