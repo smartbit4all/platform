bad28535eadebc6d27a9aa8075f70a22e1755263
Attila Mate <attila.mate@it4all.hu>
2021-12-14 08:46:55 +0100

    [#5813] remove unused UserSelectorDialog and descendants

--- ui-common/src/main/java/org/smartbit4all/ui/common/userselector/impl/UserSelectorViewModelConfiguration.java
+++ ui-common/src/main/java/org/smartbit4all/ui/common/userselector/impl/UserSelectorViewModelConfiguration.java
@@ -3,9 +3,7 @@ package org.smartbit4all.ui.common.userselector.impl;
 import java.util.Map;
 import org.smartbit4all.api.org.OrgApi;
 import org.smartbit4all.core.object.ApiBeanDescriptor;
-import org.smartbit4all.ui.api.userselector.UserMultiSelectorViewModel;
 import org.smartbit4all.ui.api.userselector.UserSelectorViewModel;
-import org.smartbit4all.ui.api.userselector.UserSingleSelectorViewModel;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,18 +18,6 @@ public class UserSelectorViewModelConfiguration {
   public UserSelectorViewModelConfiguration() {
     userSelectorDescriptor = UserSelectorViewModelUtil.createUserSelectorBean();
     commandsDescriptor = UserSelectorViewModelUtil.createCommandsBean();
-  }
-
-  @Bean
-  @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-  public UserSingleSelectorViewModel userSingleSelectorViewModel(OrgApi orgApi) {
-    return new UserSingleSelectorViewModelImpl(orgApi, userSelectorDescriptor);
-  }
-
-  @Bean
-  @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-  public UserMultiSelectorViewModel userMultiSelectorViewModel(OrgApi orgApi) {
-    return new UserMultiSelectorViewModelImpl(orgApi, userSelectorDescriptor, commandsDescriptor);
   }
 
   @Bean
