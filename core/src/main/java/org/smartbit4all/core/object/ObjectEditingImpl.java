5e93d71a518d2215b45c3bacb4c9d302796410b5
Balazs Horvath <balazs.horvath@it4allhungary.onmicrosoft.com>
2021-12-17 08:06:13 +0100

    [#5879] add getParam method to object editing

--- core/src/main/java/org/smartbit4all/core/object/ObjectEditingImpl.java
+++ core/src/main/java/org/smartbit4all/core/object/ObjectEditingImpl.java
@@ -44,4 +44,28 @@ public class ObjectEditingImpl implements ObjectEditing {
     }
   }
 
+  /**
+   * Returns the parameter with the given index from the params array. Checks the param's type with
+   * the given one and casts it.
+   */
+  protected <T> T getParam(Object[] params, int idx, Class<T> type) {
+    if (params == null || params.length - 1 < idx) {
+      String msg = String
+          .format("The given index [%d] is out of the param range [%d]", idx, params.length);
+      throw new IndexOutOfBoundsException(msg);
+    }
+    Object param = params[idx];
+    if (param == null) {
+      // FIXME assert if type is Void.class and throw exception if not?
+      return null;
+    }
+    if (type.isAssignableFrom(param.getClass())) {
+      return type.cast(param);
+    } else {
+      String msg = String.format("The given type [%s] can not be assigned from the param type [%s]",
+          type.getName(), param.getClass().getName());
+      throw new IllegalArgumentException(msg);
+    }
+  }
+
 }
