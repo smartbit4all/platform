4c3df4f2f9ab80d48fefacdc46e99aac7484a631
Zoltan Suller <zoltan.suller@it4all.hu>
2022-06-15 14:29:54 +0200

    [7903] InvocationApi test fix

--- api/src/test/java/org/smartbit4all/api/invocation/bean/TestDataBean.java
+++ api/src/test/java/org/smartbit4all/api/invocation/bean/TestDataBean.java
@@ -1,20 +1,7 @@
 package org.smartbit4all.api.invocation.bean;
 
 import java.net.URI;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.annotation.JsonTypeName;
-import io.swagger.annotations.ApiModelProperty;
 
-@JsonPropertyOrder({
-    TestDataBean.URI,
-    TestDataBean.DATA,
-    TestDataBean.BOOL
-})
-@JsonTypeName("TestDataBean")
 public class TestDataBean {
 
   public static final String URI = "uri";
@@ -26,39 +13,19 @@ public class TestDataBean {
   public static final String BOOL = "bool";
   private boolean bool;
 
-  /**
-   * Get uri
-   * 
-   * @return uri
-   **/
-  @NotNull
-  @Valid
-  @ApiModelProperty(required = true, value = "")
-  @JsonProperty(URI)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
-
   public URI getUri() {
     return uri;
   }
 
 
-  @JsonProperty(URI)
-  @JsonInclude(value = JsonInclude.Include.ALWAYS)
   public void setUri(URI uri) {
     this.uri = uri;
   }
 
-  @javax.annotation.Nullable
-  @Valid
-  @ApiModelProperty(value = "")
-  @JsonProperty(DATA)
-  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
   public String getData() {
     return data;
   }
 
-  @JsonProperty(DATA)
-  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
   public void setData(String data) {
     this.data = data;
   }
@@ -68,17 +35,10 @@ public class TestDataBean {
     return this;
   }
 
-  @javax.annotation.Nullable
-  @Valid
-  @ApiModelProperty(value = "")
-  @JsonProperty(BOOL)
-  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
   public boolean isBool() {
     return bool;
   }
 
-  @JsonProperty(BOOL)
-  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
   public void setBool(boolean bool) {
     this.bool = bool;
   }
