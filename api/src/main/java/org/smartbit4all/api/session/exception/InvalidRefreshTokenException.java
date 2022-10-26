807b0c8780a9e18f0b993544cd7bc48f74b0d50b
horvathb <balazs.horvath@it4all.hu>
2022-10-26 14:00:35 +0200

    [#8720] fix invalid refresh token error code

--- api/src/main/java/org/smartbit4all/api/session/exception/InvalidRefreshTokenException.java
+++ api/src/main/java/org/smartbit4all/api/session/exception/InvalidRefreshTokenException.java
@@ -4,7 +4,7 @@ public class InvalidRefreshTokenException extends SessionException {
 
   private static final long serialVersionUID = 3921042424847051111L;
 
-  private static final String ERROR_CODE = "The given refresh token is invalid!";
+  private static final String ERROR_CODE = "session.refreshtoken.invalid";
   private static final String ERROR_MSG = "The given refresh token is invalid!";
 
   public InvalidRefreshTokenException() {
