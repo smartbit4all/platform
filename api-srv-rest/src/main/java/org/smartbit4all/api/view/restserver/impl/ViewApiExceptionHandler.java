0b4ba1fdfdb5333e058e6e71ecb3692e23cea8df
Attila Mate <attila.mate@it4all.hu>
2022-11-11 13:34:44 +0100

    [#8922] create ViewApiError and ViewApiExceptionHandler

---
+++ api-srv-rest/src/main/java/org/smartbit4all/api/view/restserver/impl/ViewApiExceptionHandler.java
@@ -0,0 +1,24 @@
+package org.smartbit4all.api.view.restserver.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smartbit4all.api.view.bean.ViewApiError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+// @ControllerAdvice(assignableTypes = {ViewApiMarker.class})
+public class ViewApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+  private static final Logger log = LoggerFactory.getLogger(ViewApiExceptionHandler.class);
+
+  @ExceptionHandler(RuntimeException.class)
+  protected ResponseEntity<Object> handleViewApiException(RuntimeException ex) {
+    log.debug("Handling runtime error", ex);
+    return ResponseEntity.status(HttpStatus.CONFLICT)
+        .body(new ViewApiError()
+            .message(ex.getMessage()));
+  }
+
+}
