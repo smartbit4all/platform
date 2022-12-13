661ec5bf05a05036c2219e3f6b60587b440903c5
Attila Mate <attila.mate@it4all.hu>
2022-12-13 15:20:36 +0100

    [#9286] Values.completeValues

--- api/src/main/java/org/smartbit4all/api/value/Values.java
+++ api/src/main/java/org/smartbit4all/api/value/Values.java
@@ -1,5 +1,6 @@
 package org.smartbit4all.api.value;
 
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 import java.net.URI;
 import java.util.List;
@@ -43,14 +44,34 @@ public class Values {
         .collect(Collectors.toList());
   }
 
+  public static final List<Value> completeValues(ObjectApi objectApi, List<Value> allValues,
+      List<URI> uris, String... displayValuePath) {
+    List<URI> allUris = allValues.stream()
+        .map(Value::getObjectUri)
+        .collect(toList());
+    uris.forEach(uri -> {
+      URI latestUri = objectApi.getLatestUri(uri);
+      if (!allUris.contains(latestUri)) {
+        allValues.add(loadValue(objectApi, uri, displayValuePath));
+      }
+    });
+    return allValues;
+  }
+
   private static final Value findOrLoadValue(ObjectApi objectApi, Map<URI, Value> allTemplates,
       URI u, String... paths) {
     Value v = allTemplates.get(u);
     if (v == null) {
-      v = new Value()
-          .objectUri(u)
-          .displayValue(objectApi.load(u).getValue(String.class, paths));
+      v = loadValue(objectApi, u, paths);
     }
+    return v;
+  }
+
+  private static Value loadValue(ObjectApi objectApi, URI u, String... paths) {
+    Value v;
+    v = new Value()
+        .objectUri(u)
+        .displayValue(objectApi.load(u).getValue(String.class, paths));
     return v;
   }
 
