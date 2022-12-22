2662dc4e37a715b21c7fd0e5ce1aaef63cec4a9d
Attila Mate <attila.mate@it4all.hu>
2022-12-22 11:35:10 +0100

    [#9377] create constants for UiAction param keys

---
+++ api/src/main/java/org/smartbit4all/api/view/UiActions.java
@@ -0,0 +1,10 @@
+package org.smartbit4all.api.view;
+
+public class UiActions {
+
+  public static final String INPUT = "input";
+
+  public static final String MODEL = "model";
+
+  private UiActions() {}
+}
