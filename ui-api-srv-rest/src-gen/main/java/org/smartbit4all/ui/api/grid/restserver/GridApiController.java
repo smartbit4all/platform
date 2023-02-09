763bf3bfbe0f9fd7e806405dc0b1797cdb9513bf
Ádám <adam.maglodi@it4all.hu>
2023-02-09 09:16:53 +0100

    [#9855] GridUIApi rest generate

---
+++ ui-api-srv-rest/src-gen/main/java/org/smartbit4all/ui/api/grid/restserver/GridApiController.java
@@ -0,0 +1,25 @@
+package org.smartbit4all.ui.api.grid.restserver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+import javax.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+@Controller
+@RequestMapping("${openapi.gridUI.base-path:/}")
+public class GridApiController implements GridApi {
+
+    private final GridApiDelegate delegate;
+
+    public GridApiController(@Autowired(required = false) GridApiDelegate delegate) {
+        this.delegate = Optional.ofNullable(delegate).orElse(new GridApiDelegate() {});
+    }
+
+    @Override
+    public GridApiDelegate getDelegate() {
+        return delegate;
+    }
+
+}
