56e0984ed6c0582fa43214b71c6f50761773e490
Peter Boros <peter.boros@it4all.hu>
2023-02-09 11:09:22 +0100

    [#9880] GridApi - use LocaleSettingApi to resolve labels

--- api/src/main/java/org/smartbit4all/api/grid/GridApiImpl.java
+++ api/src/main/java/org/smartbit4all/api/grid/GridApiImpl.java
@@ -2,14 +2,12 @@ package org.smartbit4all.api.grid;
 
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
-
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
 import org.smartbit4all.api.collection.SearchIndex;
 import org.smartbit4all.api.filterexpression.bean.FilterExpressionOrderBy;
 import org.smartbit4all.api.grid.bean.GridColumnMeta;
@@ -19,6 +17,7 @@ import org.smartbit4all.api.grid.bean.GridRow;
 import org.smartbit4all.api.grid.bean.GridView;
 import org.smartbit4all.api.grid.bean.GridViewDescriptor;
 import org.smartbit4all.api.grid.bean.GridViewDescriptor.KindEnum;
+import org.smartbit4all.api.setting.LocaleSettingApi;
 import org.smartbit4all.core.object.ObjectApi;
 import org.smartbit4all.domain.data.DataColumn;
 import org.smartbit4all.domain.data.DataRow;
@@ -28,102 +27,107 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class GridApiImpl implements GridApi {
 
-    @Autowired
-    private ObjectApi objectApi;
+  @Autowired
+  private ObjectApi objectApi;
 
-    @Autowired
-    private TableDataApi tableDataApi;
+  @Autowired
+  private TableDataApi tableDataApi;
 
-    @Override
-    public <T> GridModel modelOf(Class<T> clazz, List<T> objectList, Map<String, String> columns) {
-        GridModel result = new GridModel();
-        result.setTotalRowCount(objectList.size());
+  @Autowired
+  private LocaleSettingApi localeSettingApi;
 
-        GridViewDescriptor tableHeader = new GridViewDescriptor().kind(KindEnum.TABLE);
-        GridView tableView = new GridView().descriptor(tableHeader);
-        result.addAvailableViewsItem(tableView);
-        result.setView(tableView);
-        columns.entrySet().stream().forEach(e -> tableHeader
-                .addColumnsItem(new GridColumnMeta().label(e.getValue()).propertyName(e.getKey())));
+  @Override
+  public <T> GridModel modelOf(Class<T> clazz, List<T> objectList, Map<String, String> columns) {
+    GridModel result = new GridModel();
+    result.setTotalRowCount(objectList.size());
 
-        // AtomicInteger i = new AtomicInteger(1);
-        GridPage page = new GridPage().lowerBound(1).upperBound(objectList.size());
-        List<GridRow> gridRows = IntStream.rangeClosed(1, page.getUpperBound())
-                .mapToObj(i -> new GridRow()
-                        .id(String.valueOf(i))
-                        .data(objectApi.create(null, objectList.get(i - 1))
-                                .getObjectAsMap()
-                                .entrySet().stream()
-                                .filter(e -> columns.containsKey(e.getKey()))
-                                .collect(toMap(Entry::getKey, Entry::getValue))))
-                .collect(toList());
-        page.setRows(gridRows);
+    GridViewDescriptor tableHeader = new GridViewDescriptor().kind(KindEnum.TABLE);
+    GridView tableView = new GridView().descriptor(tableHeader);
+    result.addAvailableViewsItem(tableView);
+    result.setView(tableView);
+    columns.entrySet().stream().forEach(e -> tableHeader
+        .addColumnsItem(new GridColumnMeta().label(localeSettingApi.get(e.getValue()))
+            .propertyName(e.getKey())));
 
-        result.setPage(page);
-        return result;
+    // AtomicInteger i = new AtomicInteger(1);
+    GridPage page = new GridPage().lowerBound(1).upperBound(objectList.size());
+    List<GridRow> gridRows = IntStream.rangeClosed(1, page.getUpperBound())
+        .mapToObj(i -> new GridRow()
+            .id(String.valueOf(i))
+            .data(objectApi.create(null, objectList.get(i - 1))
+                .getObjectAsMap()
+                .entrySet().stream()
+                .filter(e -> columns.containsKey(e.getKey()))
+                .collect(toMap(Entry::getKey, Entry::getValue))))
+        .collect(toList());
+    page.setRows(gridRows);
+
+    result.setPage(page);
+    return result;
+  }
+
+  @Override
+  public GridModel modelOf(TableData<?> tableData) {
+    return constructModel(tableData);
+  }
+
+  private GridModel constructModel(TableData<?> tableData) {
+    GridModel result = new GridModel();
+    GridViewDescriptor tableHeader = new GridViewDescriptor().kind(KindEnum.TABLE);
+    GridView tableView = new GridView().descriptor(tableHeader);
+    result.addAvailableViewsItem(tableView);
+    result.setView(tableView);
+    for (DataColumn<?> column : tableData.columns()) {
+      tableHeader.addColumnsItem(
+          new GridColumnMeta().propertyName(column.getName())
+              .label(localeSettingApi.get(column.getName()))
+              .typeClass(column.getProperty().type().getName()));
     }
+    result.totalRowCount(tableData.size());
 
-    @Override
-    public GridModel modelOf(TableData<?> tableData) {
-        return constructModel(tableData);
+    result.page(
+        constructPage(tableData, 0, tableData.size()).lowerBound(1).upperBound(tableData.size()));
+
+    return result;
+  }
+
+  @Override
+  public GridModel modelOfUris(SearchIndex<?> searchIndex, Stream<URI> uris) {
+    return constructModel(searchIndex.tableDataOfUris(uris));
+  }
+
+  @Override
+  public <T> GridModel modelOfObjects(SearchIndex<T> searchIndex, Stream<T> objects) {
+    return constructModel(searchIndex.tableDataOfObjects(objects));
+  }
+
+  @Override
+  public void loadPage(GridModel model, int lowerBound, int upperBound) {
+    TableData<?> tableData =
+        tableDataApi.readPage(model.getAccessConfig().getDataUri(), lowerBound - 1,
+            upperBound);
+    model.page(constructPage(
+        tableData, 0, tableData.size())
+            .lowerBound(lowerBound).upperBound(upperBound));
+  }
+
+  private GridPage constructPage(TableData<?> tableData, int beginIndex, int endIndex) {
+    GridPage page = new GridPage();
+    for (int i = beginIndex; i < tableData.rows().size(); i++) {
+      DataRow row = tableData.rows().get(i);
+      page.addRowsItem(new GridRow().id(Integer.toString(i)).data(tableData.columns().stream()
+          .filter(c -> tableData.get(c, row) != null)
+          .collect(toMap(DataColumn::getName, c -> tableData.get(c, row)))));
     }
+    return page;
+  }
 
-    private GridModel constructModel(TableData<?> tableData) {
-        GridModel result = new GridModel();
-        GridViewDescriptor tableHeader = new GridViewDescriptor().kind(KindEnum.TABLE);
-        GridView tableView = new GridView().descriptor(tableHeader);
-        result.addAvailableViewsItem(tableView);
-        result.setView(tableView);
-        for (DataColumn<?> column : tableData.columns()) {
-            tableHeader.addColumnsItem(
-                    new GridColumnMeta().propertyName(column.getName())
-                            .typeClass(column.getProperty().type().getName()));
-        }
-        result.totalRowCount(tableData.size());
+  @Override
+  public void setColumnOrder(GridModel model, List<String> columns) {
+    model.getView().setOrderedColumnNames(columns);
+  }
 
-        result.page(
-                constructPage(tableData, 0, tableData.size()).lowerBound(1).upperBound(tableData.size()));
-
-        return result;
-    }
-
-    @Override
-    public GridModel modelOfUris(SearchIndex<?> searchIndex, Stream<URI> uris) {
-        return constructModel(searchIndex.tableDataOfUris(uris));
-    }
-
-    @Override
-    public <T> GridModel modelOfObjects(SearchIndex<T> searchIndex, Stream<T> objects) {
-        return constructModel(searchIndex.tableDataOfObjects(objects));
-    }
-
-    @Override
-    public void loadPage(GridModel model, int lowerBound, int upperBound) {
-        TableData<?> tableData = tableDataApi.readPage(model.getAccessConfig().getDataUri(), lowerBound - 1,
-                upperBound);
-        model.page(constructPage(
-                tableData, 0, tableData.size())
-                .lowerBound(lowerBound).upperBound(upperBound));
-    }
-
-    private GridPage constructPage(TableData<?> tableData, int beginIndex, int endIndex) {
-        GridPage page = new GridPage();
-        for (int i = beginIndex; i < tableData.rows().size(); i++) {
-            DataRow row = tableData.rows().get(i);
-            page.addRowsItem(new GridRow().id(Integer.toString(i)).data(tableData.columns().stream()
-                    .filter(c -> tableData.get(c, row) != null)
-                    .collect(toMap(DataColumn::getName, c -> tableData.get(c, row)))));
-        }
-        return page;
-    }
-
-    @Override
-    public void setColumnOrder(GridModel model, List<String> columns) {
-        model.getView().setOrderedColumnNames(columns);
-    }
-
-    @Override
-    public void setOrderBy(GridModel model, List<FilterExpressionOrderBy> orderByList) {
-    }
+  @Override
+  public void setOrderBy(GridModel model, List<FilterExpressionOrderBy> orderByList) {}
 
 }
