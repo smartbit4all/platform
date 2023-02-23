feeafd2721fe4a30f6d57c354f0b37e55332c6a4
Attila Mate <attila.mate@it4all.hu>
2023-02-23 14:13:00 +0100

    [#10139] add ApplicationRuntimeStorageConfig to default config

--- api/src/main/java/org/smartbit4all/api/config/PlatformApiScheduledConfig.java
+++ api/src/main/java/org/smartbit4all/api/config/PlatformApiScheduledConfig.java
@@ -4,17 +4,20 @@ import org.smartbit4all.api.binarydata.BinaryDataMaintenanceApi;
 import org.smartbit4all.api.binarydata.BinaryDataMaintenanceApiImpl;
 import org.smartbit4all.api.invocation.InvocationRegisterApi;
 import org.smartbit4all.api.invocation.InvocationRegisterApiIml;
+import org.smartbit4all.domain.config.ApplicationRuntimeStorageConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * The smartbit4all platform api config for scheduled apis.
- * 
+ *
  * @author Peter Boros
  */
 @Configuration
 @EnableScheduling
+@Import(ApplicationRuntimeStorageConfig.class)
 public class PlatformApiScheduledConfig {
 
   @Bean
