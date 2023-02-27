7b97c497729a6067a9fc367c9f30a1deedb06fd2
Attila Mate <attila.mate@it4all.hu>
2023-02-27 17:50:08 +0100

    [#10139] improve filter chain logging

--- api-srv-rest/src/main/java/org/smartbit4all/api/session/restserver/config/FilterChainExceptionHandler.java
+++ api-srv-rest/src/main/java/org/smartbit4all/api/session/restserver/config/FilterChainExceptionHandler.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smartbit4all.api.session.exception.SessionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,7 +15,8 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 
 public class FilterChainExceptionHandler extends OncePerRequestFilter {
 
-  private final Logger log = LoggerFactory.getLogger(getClass());
+  private static final Logger log = LoggerFactory.getLogger(FilterChainExceptionHandler.class);
+
 
   @Autowired
   @Qualifier("handlerExceptionResolver")
@@ -28,10 +30,12 @@ public class FilterChainExceptionHandler extends OncePerRequestFilter {
     try {
       filterChain.doFilter(request, response);
     } catch (Exception e) {
-      log.info(
-          "Caught Spring Security Filter Chain Exception. Trying to resolve... Error message: {}",
-          e.getMessage());
-      log.warn("Error trace:", e);
+      String msg = "Caught Spring Security Filter Chain Exception. Trying to resolve...";
+      if (e instanceof SessionException) {
+        log.debug(msg, e);
+      } else {
+        log.warn(msg, e);
+      }
       resolver.resolveException(request, response, null, e);
     }
   }
