60013dc93fbe1cd4d8d76d86c5a56ffe8a7af4b2
Zoltan Suller <zoltan.suller@it4all.hu>
2023-02-27 20:57:55 +0100

    [#10034] CompareApi - compare list properties

--- api/src/main/java/org/smartbit4all/api/object/CompareApiImpl.java
+++ api/src/main/java/org/smartbit4all/api/object/CompareApiImpl.java
@@ -12,6 +12,7 @@ import org.smartbit4all.api.object.bean.PropertyChangeData;
 import org.smartbit4all.api.object.bean.ReferenceChangeData;
 import org.smartbit4all.core.object.ObjectApi;
 import org.smartbit4all.core.object.ObjectNode;
+import org.smartbit4all.core.object.ObjectNodeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CompareApiImpl implements CompareApi {
@@ -22,8 +23,21 @@ public class CompareApiImpl implements CompareApi {
   @Override
   public ObjectChangeData changes(ObjectNode node1, ObjectNode node2) {
     Map<String, Object> map1 = node1 == null ? new HashMap<>() : node1.getObjectAsMap();
+    checkReferences(map1, node1);
     Map<String, Object> map2 = node2 == null ? new HashMap<>() : node2.getObjectAsMap();
+    checkReferences(map2, node2);
     return changesOfMap(map1, map2);
+  }
+
+  // if the ObjectNode has references then use those instead of the reference URIs
+  private void checkReferences(Map<String, Object> map, ObjectNode node) {
+    if (node != null) {
+      for (Entry<String, ObjectNodeReference> referenceEntries : node.getReferences().entrySet()) {
+        if (referenceEntries.getValue().get() != null) {
+          map.put(referenceEntries.getKey(), referenceEntries.getValue().get().getObjectAsMap());
+        }
+      }
+    }
   }
 
   private final ObjectChangeData changesOfMap(Map<String, Object> map1,
@@ -46,6 +60,8 @@ public class CompareApiImpl implements CompareApi {
       String subPath = pathOf(key);
       if (value1 instanceof Map || value2 instanceof Map) {
         result.addReferencesItem(referenceChangeOf(value1, value2, subPath));
+      } else if (value1 instanceof List || value2 instanceof List) {
+        result.addReferencesItem(listChangeOf(value1, value2, subPath));
       } else {
         if (!Objects.deepEquals(value1, value2)) {
           result.addPropertiesItem(
@@ -57,6 +73,8 @@ public class CompareApiImpl implements CompareApi {
       Object value1 = map1.get(key);
       if (value1 instanceof Map) {
         result.addReferencesItem(referenceChangeOf(value1, null, pathOf(key)));
+      } else if (value1 instanceof List) {
+        result.addReferencesItem(listChangeOf(value1, null, pathOf(key)));
       } else {
         if (value1 != null) {
           result.addPropertiesItem(
@@ -70,6 +88,8 @@ public class CompareApiImpl implements CompareApi {
       Object value2 = entry.getValue();
       if (value2 instanceof Map) {
         result.addReferencesItem(referenceChangeOf(null, value2, pathOf(entry.getKey())));
+      } else if (value2 instanceof List) {
+        result.addReferencesItem(listChangeOf(null, value2, pathOf(entry.getKey())));
       } else {
         if (value2 != null) {
           result.addPropertiesItem(
@@ -81,6 +101,50 @@ public class CompareApiImpl implements CompareApi {
     return result;
   }
 
+  /**
+   * Creates ObjectChangeData for two lists. The path will be the object's index in the list.
+   * 
+   * @return
+   */
+  private ObjectChangeData changesOfList(List<Object> list1,
+      List<Object> list2) {
+    ObjectChangeData result = new ObjectChangeData();
+    for (int i = 0; i < list1.size(); ++i) {
+      Object value1 = list1.get(i);
+      Object value2 = list2.size() > i ? list2.get(i) : null;
+      String subPath = String.valueOf(i);
+      if (value1 instanceof Map || value2 instanceof Map) {
+        result.addReferencesItem(referenceChangeOf(value1, value2, subPath));
+      } else if (value1 instanceof List || value2 instanceof List) {
+        result.addReferencesItem(listChangeOf(value1, value2, subPath));
+      } else {
+        if (!Objects.deepEquals(value1, value2)) {
+          result.addPropertiesItem(
+              new PropertyChangeData().path(subPath).oldValue(value1).newValue(value2));
+        }
+      }
+    }
+
+    if (list2.size() > list1.size()) {
+      for (int i = list1.size(); i < list2.size(); ++i) {
+        Object value2 = list2.get(i);
+
+        String subPath = String.valueOf(i);
+        if (value2 instanceof Map) {
+          result.addReferencesItem(referenceChangeOf(null, value2, subPath));
+        } else if (value2 instanceof List) {
+          result.addReferencesItem(listChangeOf(null, value2, subPath));
+        } else {
+          result.addPropertiesItem(
+              new PropertyChangeData().path(subPath).oldValue(null).newValue(value2));
+        }
+
+      }
+    }
+
+    return result;
+  }
+
   @SuppressWarnings("unchecked")
   private final ReferenceChangeData referenceChangeOf(Object value1, Object value2,
       String subPath) {
@@ -88,6 +152,15 @@ public class CompareApiImpl implements CompareApi {
         .objectChange(changesOfMap(
             value1 instanceof Map ? (Map<String, Object>) value1 : Collections.emptyMap(),
             value2 instanceof Map ? (Map<String, Object>) value2 : Collections.emptyMap()));
+  }
+
+  @SuppressWarnings("unchecked")
+  private final ReferenceChangeData listChangeOf(Object value1, Object value2,
+      String subPath) {
+    return new ReferenceChangeData().path(subPath)
+        .objectChange(changesOfList(
+            value1 instanceof List ? (List<Object>) value1 : Collections.emptyList(),
+            value2 instanceof List ? (List<Object>) value2 : Collections.emptyList()));
   }
 
   private static final String pathOf(String path) {
