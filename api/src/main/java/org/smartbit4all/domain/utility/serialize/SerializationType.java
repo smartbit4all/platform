037e274aa7b675e7da7fcfca83cc3d803b7faa28
Zoltan Suller <zoltan.suller@it4all.hu>
2023-03-08 09:18:10 +0100

    [#10340] UUID serialization type

--- api/src/main/java/org/smartbit4all/domain/utility/serialize/SerializationType.java
+++ api/src/main/java/org/smartbit4all/domain/utility/serialize/SerializationType.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.function.Function;
 import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
@@ -43,6 +44,7 @@ public class SerializationType<T> {
 //  OFFSETDATE    (0x1a),
   public static final SerializationType<OffsetDateTime> OFFSETDATETIME = new SerializationType<>(0x1b, OffsetDateTime.class, SerializationType::serializeOffsetDateTime, SerializationType::deserializeOffsetDateTime);
   public static final SerializationType<URI> URITYPE = new SerializationType<>(0x1c, URI.class, SerializationType::serializeUri, SerializationType::deserializeUri);
+  public static final SerializationType<UUID> UUIDTYPE = new SerializationType<>(0x1c, UUID.class, SerializationType::serializeUUID, SerializationType::deserializeUUID);
   public static final SerializationType<Boolean> BOOLEAN = new SerializationType<>(0x1d, Boolean.class, SerializationType::serializeBoolean, SerializationType::deserializeBoolean);
   public static final SerializationType<Void> OTHER = new SerializationType<>(0x40, Void.class, null, b -> null);
   // @formatter:on
@@ -120,8 +122,16 @@ public class SerializationType<T> {
     return uri.toString().getBytes(UTF8_CHARSET);
   }
 
+  private static byte[] serializeUUID(UUID uuid) {
+    return uuid.toString().getBytes(UTF8_CHARSET);
+  }
+
   private static URI deserializeUri(byte[] b) {
     return URI.create(new String(b, UTF8_CHARSET));
+  }
+
+  private static UUID deserializeUUID(byte[] b) {
+    return UUID.fromString(new String(b, UTF8_CHARSET));
   }
 
   private static byte[] serializeLocalDate(LocalDate localDate) {
