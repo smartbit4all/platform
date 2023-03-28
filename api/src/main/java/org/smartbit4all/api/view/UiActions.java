cef0fb3deec7fdf271f0a0cc5eefc7e5a253f01c
amate <amate@IT4-2023-01>
2023-03-28 17:30:08 +0200

    [#10466] add constants to UiActions

--- api/src/main/java/org/smartbit4all/api/view/UiActions.java
+++ api/src/main/java/org/smartbit4all/api/view/UiActions.java
@@ -6,5 +6,9 @@ public class UiActions {
 
   public static final String MODEL = "model";
 
+  public static final String ITEM = "item";
+
+  public static final String URI = "uri";
+
   private UiActions() {}
 }
