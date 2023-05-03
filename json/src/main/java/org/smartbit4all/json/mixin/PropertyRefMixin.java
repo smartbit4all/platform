64250e0ea8b236f873366c3617411a47bb3ba02f
Attila Mate <attila.mate@it4all.hu>
2023-05-03 06:41:09 +0200

    [#11115] fix pipeline

--- json/src/main/java/org/smartbit4all/json/mixin/PropertyRefMixin.java
+++ json/src/main/java/org/smartbit4all/json/mixin/PropertyRefMixin.java
@@ -1,26 +1,21 @@
 /*******************************************************************************
  * Copyright (C) 2020 - 2020 it4all Hungary Kft.
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package org.smartbit4all.json.mixin;
 
-import org.smartbit4all.json.deserializer.PropertyRefDeserializer;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonTypeName("PropertyRef")
-@JsonDeserialize(using = PropertyRefDeserializer.class)
 public abstract class PropertyRefMixin extends PropertyMixin {
 }
