a39e71bf8bc5993b60a40afa6dbb4912deac8871
Attila Mate <attila.mate@it4all.hu>
2023-05-05 18:51:17 +0200

    [#11065] PropertyRef now works with PropertyObject as referredProperty

--- api/src/main/java/org/smartbit4all/domain/meta/PropertyRef.java
+++ api/src/main/java/org/smartbit4all/domain/meta/PropertyRef.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
  * Copyright (C) 2020 - 2020 it4all Hungary Kft.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -22,7 +22,7 @@ import org.smartbit4all.core.utility.StringConstant;
 /**
  * This property refers another one that is owned by a related entity. It contains the list of
  * {@link Reference} that defines how to joins the given entity.
- * 
+ *
  * @author Peter Boros
  *
  * @param <T>
@@ -40,20 +40,24 @@ public class PropertyRef<T> extends Property<T> {
   /**
    * This is the referred property. It can be any kind ({@link PropertyOwned}, {@link PropertyRef},
    * {@link PropertyComputed}).
-   * 
+   *
    */
   private Property<T> referredProperty;
 
   /**
    * Creates a reference property, where the reference is through the join path, to the
    * referredProperty.
-   * 
+   *
    * @param joinPath
    * @param referredProperty
    */
   public PropertyRef(String name, List<Reference<?, ?>> joinPath, Property<T> referredProperty) {
     super((name == null || name.isEmpty()) ? PropertyRef.constructName(joinPath, referredProperty)
         : name, referredProperty.type(), referredProperty.jdbcConverter());
+    this.referredProperty = referredProperty;
+    if (referredProperty instanceof PropertyObject) {
+      referredProperty = (Property<T>) ((PropertyObject) referredProperty).getBasic();
+    }
     if (referredProperty instanceof PropertyOwned || referredProperty instanceof PropertyComputed) {
       this.joinPath = new JoinPath(joinPath);
     } else if (referredProperty instanceof PropertyRef) {
@@ -63,13 +67,12 @@ public class PropertyRef<T> extends Property<T> {
       allPath.addAll(secondPart);
       this.joinPath = new JoinPath(allPath);
     }
-    this.referredProperty = referredProperty;
   }
 
   /**
    * This utility construct the name of a referred property from the join path and the name of the
    * finally referred property. The names are separated with dot.
-   * 
+   *
    * @param joinPath
    * @param referredProperty
    * @return
@@ -98,7 +101,7 @@ public class PropertyRef<T> extends Property<T> {
 
   /**
    * The references which lead to this property.
-   * 
+   *
    * @return
    */
   public final List<Reference<?, ?>> getJoinReferences() {
@@ -108,7 +111,7 @@ public class PropertyRef<T> extends Property<T> {
   /**
    * This is the referred property from the end of the join path. It can be a {@link PropertyOwned},
    * or a {@link PropertyRef} as well. The referred property would be merged into this join.
-   * 
+   *
    * @return
    */
   public final Property<T> getReferredProperty() {
@@ -117,14 +120,20 @@ public class PropertyRef<T> extends Property<T> {
 
   /**
    * Returns the owned property at the end of the reference chain (join path).
-   * 
+   *
    * @return
    */
   public final PropertyOwned<T> getReferredOwnedProperty() {
-    if (referredProperty instanceof PropertyOwned) {
-      return (PropertyOwned<T>) referredProperty;
-    } else if (referredProperty instanceof PropertyRef) {
-      return ((PropertyRef<T>) referredProperty).getReferredOwnedProperty();
+    Property<T> prop;
+    if (referredProperty instanceof PropertyObject) {
+      prop = (Property<T>) ((PropertyObject) referredProperty).getBasic();
+    } else {
+      prop = referredProperty;
+    }
+    if (prop instanceof PropertyOwned) {
+      return (PropertyOwned<T>) prop;
+    } else if (prop instanceof PropertyRef) {
+      return ((PropertyRef<T>) prop).getReferredOwnedProperty();
     } else {
       // TODO computed property has no single owned property
       return null;
@@ -133,7 +142,7 @@ public class PropertyRef<T> extends Property<T> {
 
   /**
    * The join path is a wrapper object containing the references which lead to this property.
-   * 
+   *
    * @return
    */
   public JoinPath getJoinPath() {
