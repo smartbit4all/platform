febb97bea83dd469924da6555da6ea957eb936b5
Balázs Horváth <balazs.horvath@it4all.hu>
2023-05-23 15:26:51 +0200

    [#11439] introduce GridModels utility class

---
+++ api/src/main/java/org/smartbit4all/api/view/grid/GridModels.java
@@ -0,0 +1,27 @@
+package org.smartbit4all.api.view.grid;
+
+import java.util.Objects;
+import java.util.Optional;
+import org.smartbit4all.api.grid.bean.GridModel;
+import org.smartbit4all.api.grid.bean.GridRow;
+
+/**
+ * {@link GridModel} static utility method collection
+ */
+public class GridModels {
+
+  private GridModels() {}
+
+  @SuppressWarnings("null")
+  public static Optional<GridRow> findGridRowById(GridModel gridModel, String rowId) {
+    Objects.requireNonNull(rowId, "rowId can not be null!");
+    Objects.requireNonNull(gridModel, "gridModel can not be null!");
+    Objects.requireNonNull(gridModel.getPage(), "gridModel.getPage() can not be null!");
+    Objects.requireNonNull(gridModel.getPage().getRows(),
+        "gridModel.getPage().getRows() can not be null!");
+
+    return gridModel.getPage().getRows().stream()
+        .filter(row -> Objects.equals(row.getId(), rowId)).findFirst();
+  }
+
+}
