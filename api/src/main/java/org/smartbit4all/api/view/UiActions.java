2af7c185b1d266f4300e8a446ec3789e8e4c4fbb
Attila Mate <attila.mate@it4all.hu>
2023-06-08 14:10:39 +0200

    [#11698] add builder to UiActions

--- api/src/main/java/org/smartbit4all/api/view/UiActions.java
+++ api/src/main/java/org/smartbit4all/api/view/UiActions.java
@@ -1,5 +1,9 @@
 package org.smartbit4all.api.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.smartbit4all.api.view.bean.UiAction;
+
 public class UiActions {
 
   public static final String INPUT = "input";
@@ -13,4 +17,42 @@ public class UiActions {
   public static final String URI = "uri";
 
   private UiActions() {}
+
+  public static UiActionsBuilder builder() {
+    return new UiActionsBuilder();
+  }
+
+  public static class UiActionsBuilder {
+
+    private final List<UiAction> actions;
+
+    private UiActionsBuilder() {
+      this.actions = new ArrayList<>();
+    }
+
+    public UiActionsBuilder add(String action) {
+      return add(new UiAction().code(action));
+    }
+
+    public UiActionsBuilder add(UiAction action) {
+      this.actions.add(action);
+      return this;
+    }
+
+    public UiActionsBuilder addIf(String action, boolean... conditions) {
+      return addIf(new UiAction().code(action), conditions);
+    }
+
+    public UiActionsBuilder addIf(UiAction action, boolean... conditions) {
+      for (boolean c : conditions) {
+        if (!c)
+          return this;
+      }
+      return this.add(action);
+    }
+
+    public List<UiAction> build() {
+      return this.actions;
+    }
+  }
 }
