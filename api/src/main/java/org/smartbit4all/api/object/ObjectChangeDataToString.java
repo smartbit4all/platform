2bb945bc5bdffb1c256429327a0ee2654494c7c8
Attila Mate <attila.mate@it4all.hu>
2023-07-27 11:45:32 +0200

    [#12819] display formatted datetime in systemlog

--- api/src/main/java/org/smartbit4all/api/object/ObjectChangeDataToString.java
+++ api/src/main/java/org/smartbit4all/api/object/ObjectChangeDataToString.java
@@ -1,34 +1,43 @@
 package org.smartbit4all.api.object;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import org.smartbit4all.api.object.bean.ObjectChangeData;
 import org.smartbit4all.api.object.bean.PropertyChangeData;
 import org.smartbit4all.api.object.bean.ReferenceChangeData;
 import org.smartbit4all.api.setting.LocaleSettingApi;
+import org.smartbit4all.core.object.ObjectApi;
 
 public class ObjectChangeDataToString {
 
-  public static String toFormattedString(LocaleSettingApi localeSettingApi,
+  private static DateTimeFormatter dateTimeFormatter =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+  public static String toFormattedString(ObjectApi objectApi, LocaleSettingApi localeSettingApi,
       ObjectChangeData change, boolean htmlFormatted) {
     StringBuilder sb = new StringBuilder();
-    formatProperties(localeSettingApi, sb, "", change.getProperties(), htmlFormatted);
-    formatReferences(localeSettingApi, sb, "", change.getReferences(), htmlFormatted);
+    formatProperties(objectApi, localeSettingApi, sb, "", change.getProperties(), htmlFormatted);
+    formatReferences(objectApi, localeSettingApi, sb, "", change.getReferences(), htmlFormatted);
     return sb.toString();
   }
 
-  private static void formatReferences(LocaleSettingApi localeSettingApi, StringBuilder sb,
+  private static void formatReferences(ObjectApi objectApi, LocaleSettingApi localeSettingApi,
+      StringBuilder sb,
       String prefix, List<ReferenceChangeData> references, boolean htmlFormatted) {
     for (ReferenceChangeData referenceChangeData : references) {
       String newPrefix = prefix + referenceChangeData.getPath() + ".";
-      formatProperties(localeSettingApi, sb, newPrefix,
+      formatProperties(objectApi, localeSettingApi, sb, newPrefix,
           referenceChangeData.getObjectChange().getProperties(), htmlFormatted);
-      formatReferences(localeSettingApi, sb, newPrefix,
+      formatReferences(objectApi, localeSettingApi, sb, newPrefix,
           referenceChangeData.getObjectChange().getReferences(), htmlFormatted);
     }
   }
 
-  private static void formatProperties(LocaleSettingApi localeSettingApi, StringBuilder sb,
+  private static void formatProperties(ObjectApi objectApi, LocaleSettingApi localeSettingApi,
+      StringBuilder sb,
       String prefix, List<PropertyChangeData> properties, boolean htmlFormatted) {
     for (PropertyChangeData propertyChangeData : properties) {
       String path = prefix + propertyChangeData.getPath();
@@ -41,30 +50,48 @@ public class ObjectChangeDataToString {
       if (!translations.isEmpty()) {
         path += " " + translations;
       }
+      Object oldValue = convertOffsetDatetime(objectApi, propertyChangeData.getOldValue());
+      Object newValue = convertOffsetDatetime(objectApi, propertyChangeData.getNewValue());
       if (htmlFormatted) {
         sb.append("<b>")
             .append(path)
             .append("</b>")
             .append(": ");
-        if (propertyChangeData.getOldValue() != null) {
+        if (oldValue != null) {
           sb
-              .append(propertyChangeData.getOldValue())
+              .append(oldValue)
               .append("<b>")
               .append(" -> ")
               .append("</b>");
         }
         sb
-            .append(propertyChangeData.getNewValue())
+            .append(newValue)
             .append("<br>");
       } else {
         sb.append(path)
             .append(": ")
-            .append(propertyChangeData.getOldValue())
+            .append(oldValue)
             .append("\n")
             .append(" -> ")
-            .append(propertyChangeData.getNewValue());
+            .append(newValue);
       }
     }
+  }
+
+  private static Object convertOffsetDatetime(ObjectApi objectApi, Object date) {
+    if (date == null) {
+      return date;
+    }
+    try {
+      OffsetDateTime offsetDateTime = objectApi.asType(OffsetDateTime.class, date);
+      if (offsetDateTime != null) {
+        return dateTimeFormatter
+            .format(offsetDateTime.atZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime());
+      }
+    } catch (Exception e) {
+    }
+    return date;
+
   }
 
   /**
