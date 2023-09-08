8e26d729f4e7caac7deb4dcca8a7886d49c9f859
Daniel Papp <daniel.papp@it4all.hu>
2023-09-08 16:44:59 +0200

    [#13370] add AclEditingPageApi

---
+++ bff-api/src/main/java/org/smartbit4all/bff/api/acl/AclEditingPageApi.java
@@ -0,0 +1,20 @@
+package org.smartbit4all.bff.api.acl;
+
+import java.util.UUID;
+import org.smartbit4all.api.org.bean.ACL;
+import org.smartbit4all.api.view.PageApi;
+import org.smartbit4all.api.view.annotation.ActionHandler;
+import org.smartbit4all.api.view.bean.UiActionRequest;
+
+public interface AclEditingPageApi extends PageApi<ACL> {
+  String CANCEL = "CANCEL";
+
+  @ActionHandler(CANCEL)
+  void closeAclEditing(UUID viewUuid, UiActionRequest request);
+
+  String SAVE = "SAVE";
+
+  @ActionHandler(SAVE)
+  void saveEditing(UUID viewUuid, UiActionRequest request);
+
+}
