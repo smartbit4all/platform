09f855ccb004e92e28ebfd7f20a2618e60292d53
Szabolcs Bazil Papp <szabolcs.papp@it4all.hu>
2023-09-29 15:43:29 +0200

    [#13357] AssociationGridConfig now contains the MDM Entry Descriptor used to reach the enumeration of selectable new associations, adding a new association through the provided auxilliary action now works.

--- bff-api/src/main/java/org/smartbit4all/bff/api/assoc/AssociationGridConfig.java
+++ bff-api/src/main/java/org/smartbit4all/bff/api/assoc/AssociationGridConfig.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 import org.smartbit4all.api.collection.CollectionApi;
 import org.smartbit4all.api.collection.SearchIndex;
 import org.smartbit4all.api.invocation.bean.InvocationRequest;
+import org.smartbit4all.api.mdm.bean.MDMEntryDescriptor;
 import org.smartbit4all.api.object.bean.RefObject;
 import com.google.common.base.Strings;
 
@@ -17,8 +18,14 @@ public final class AssociationGridConfig {
   }
 
   private final String name;
+  private final String mdmDefinition;
+  /** The descriptor enumerating the possible new values for the assoc list. */
+  private final MDMEntryDescriptor entryDescriptor;
   private final SearchIndexIdentifier searchIndex;
+  /** Where the assoc list is in the domain object. */
   private final String[] pathToAssocList;
+  /** Where the assoc list is in the view model. */
+  private final String[] assocListInView;
   private final List<String> columnsToInclude;
   private final List<String> columnsToShow;
 
@@ -27,23 +34,42 @@ public final class AssociationGridConfig {
 
   private final List<String> supportedWidgetActions;
 
-  private AssociationGridConfig(String name, SearchIndexIdentifier searchIndex,
+  private final String newAssocDialogName;
+
+  private AssociationGridConfig(String name,
+      String mdmDefinition,
+      MDMEntryDescriptor entryDescriptor,
+      SearchIndexIdentifier searchIndex,
       String[] pathToAssocList,
+      String[] assocListInView,
       List<String> columnsToInclude, List<String> columnsToShow,
-      InvocationRequest onGridPageRenderCallback, InvocationRequest onGridSelectionChangedCallback,
-      List<String> supportedWidgetActions) {
+      InvocationRequest onGridPageRenderCallback,
+      InvocationRequest onGridSelectionChangedCallback,
+      List<String> supportedWidgetActions, String newAssocDialogName) {
     this.name = name;
+    this.mdmDefinition = mdmDefinition;
+    this.entryDescriptor = entryDescriptor;
     this.searchIndex = searchIndex;
     this.pathToAssocList = pathToAssocList;
+    this.assocListInView = assocListInView;
     this.columnsToInclude = columnsToInclude;
     this.columnsToShow = columnsToShow;
     this.onGridPageRenderCallback = onGridPageRenderCallback;
     this.onGridSelectionChangedCallback = onGridSelectionChangedCallback;
     this.supportedWidgetActions = supportedWidgetActions;
+    this.newAssocDialogName = newAssocDialogName;
   }
 
   String name() {
     return name;
+  }
+
+  String mdmDefinition() {
+    return mdmDefinition;
+  }
+
+  MDMEntryDescriptor entryDescriptor() {
+    return entryDescriptor;
   }
 
   SearchIndex<RefObject> searchIndex(CollectionApi collectionApi) {
@@ -52,6 +78,10 @@ public final class AssociationGridConfig {
 
   public String[] pathToAssocList() {
     return Arrays.copyOf(pathToAssocList, pathToAssocList.length);
+  }
+
+  public String[] assocListInView() {
+    return Arrays.copyOf(assocListInView, assocListInView.length);
   }
 
   public List<String> columnsToInclude() {
@@ -72,6 +102,10 @@ public final class AssociationGridConfig {
 
   public List<String> supportedWidgetActions() {
     return new ArrayList<>(supportedWidgetActions);
+  }
+
+  public String getNewAssocDialogName() {
+    return newAssocDialogName;
   }
 
   static final class SearchIndexIdentifier {
@@ -95,8 +129,12 @@ public final class AssociationGridConfig {
 
   public static final class Builder {
     private final String name;
+
+    private String mdmDefinition;
+    private MDMEntryDescriptor entryDescriptor;
     private SearchIndexIdentifier searchIndex;
     private String[] pathToAssocList;
+    private String[] assocListInView;
     private List<String> columnsToInclude;
     private List<String> columnsToShow;
 
@@ -105,11 +143,23 @@ public final class AssociationGridConfig {
 
     private List<String> supportedWidgetActions;
 
+    private String newAssocDialogName;
+
     private Builder(String name) {
       if (Strings.isNullOrEmpty(name)) {
         throw new IllegalArgumentException("name cannot be null or empty!");
       }
       this.name = name;
+    }
+
+    public Builder mdmDefinition(String mdmDefinition) {
+      this.mdmDefinition = mdmDefinition;
+      return this;
+    }
+
+    public Builder entryDescriptor(MDMEntryDescriptor entryDescriptor) {
+      this.entryDescriptor = Objects.requireNonNull(entryDescriptor);
+      return this;
     }
 
     public Builder searchIndex(SearchIndex<RefObject> searchIndex) {
@@ -127,6 +177,14 @@ public final class AssociationGridConfig {
         throw new IllegalArgumentException("pathToAssocList cannot be null or empty!");
       }
       this.pathToAssocList = Arrays.copyOf(pathToAssocList, pathToAssocList.length);
+      return this;
+    }
+
+    public Builder inViewModel(String... assocListInView) {
+      if (assocListInView == null || assocListInView.length < 1) {
+        throw new IllegalArgumentException("pathToAssocList cannot be null or empty!");
+      }
+      this.assocListInView = Arrays.copyOf(assocListInView, assocListInView.length);
       return this;
     }
 
@@ -164,10 +222,18 @@ public final class AssociationGridConfig {
       return this;
     }
 
+    public Builder newAssocDialogName(String newAssocDialogName) {
+      this.newAssocDialogName = newAssocDialogName;
+      return this;
+    }
+
     public AssociationGridConfig build() {
       return new AssociationGridConfig(
-          name, searchIndex, pathToAssocList, columnsToInclude, columnsToShow,
-          onGridPageRenderCallback, onGridSelectionChangedCallback, supportedWidgetActions);
+          name, mdmDefinition, entryDescriptor, searchIndex, pathToAssocList, assocListInView,
+          columnsToInclude,
+          columnsToShow,
+          onGridPageRenderCallback, onGridSelectionChangedCallback, supportedWidgetActions,
+          newAssocDialogName);
     }
   }
 
