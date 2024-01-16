19f0fd64525c9b0863edbf29bdcc17b20060a4fe
Balázs Horváth <balazs.horvath@it4all.hu>
2024-01-16 17:15:58 +0100

    [#15618] add spring-boot-starter-jdbc to platform/sql
    and adjust SimpleDatabaseConnectionConfig to use hikari db connection
    pool as default

--- sql/src/main/java/org/smartbit4all/sql/util/SimpleDatabaseConnectionConfig.java
+++ sql/src/main/java/org/smartbit4all/sql/util/SimpleDatabaseConnectionConfig.java
@@ -1,54 +1,45 @@
 package org.smartbit4all.sql.util;
 
 import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.util.ObjectUtils;
 
+/**
+ * Makes possible to add a custom database populator.<br/>
+ * Usage: add a resource path to an .sql script in the {@value #SCRIPT_PROPERTY} property. <br/>
+ * E.g.: in application.properties add: {@value #SCRIPT_PROPERTY}=scripts/init.sql where
+ * src/main/resources/scripts/init.sql contains the database population script
+ *
+ */
 @Configuration
 public class SimpleDatabaseConnectionConfig {
 
-  @Autowired
-  private Environment env;
+  private static final String SCRIPT_PROPERTY = "smartbit4all.sql.populator.script";
 
   @Bean
-  @ConditionalOnProperty(name = "smartbit4all.sql.populator.script")
-  public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+  @ConditionalOnProperty(SCRIPT_PROPERTY)
+  public DataSourceInitializer dataSourceInitializer(DataSource dataSource,
+      @Value("${" + SCRIPT_PROPERTY + ":}") String scriptPath) throws Exception {
+
+    if (ObjectUtils.isEmpty(scriptPath)) {
+      throw new Exception("Missing property parameter: " + SCRIPT_PROPERTY);
+    }
+
     DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
     dataSourceInitializer.setDataSource(dataSource);
 
     ResourceDatabasePopulator resourceDatabasePopulator = new EmptyDatabasePopulator();
-    resourceDatabasePopulator
-        .addScript(new ClassPathResource(env.getProperty("smartbit4all.sql.populator.script")));
+    resourceDatabasePopulator.addScript(new ClassPathResource(scriptPath));
     resourceDatabasePopulator.setContinueOnError(true);
     dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
 
     return dataSourceInitializer;
   }
-
-  @Bean
-  public DataSource dataSource() {
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-    dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-    dataSource.setUrl(env.getProperty("spring.datasource.url"));
-    dataSource.setUsername(env.getProperty("spring.datasource.username"));
-    dataSource.setPassword(env.getProperty("spring.datasource.password"));
-
-    return dataSource;
-  }
-
-  @Bean
-  public JdbcTemplate dataConnection(DataSource dataSource) {
-    return new JdbcTemplate(dataSource);
-  }
-
 
 }
