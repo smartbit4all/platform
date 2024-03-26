1df51c429204030a5509a8f99b0519488e604011
Tibor Draskóczy <tibor.draskoczy@it4all.hu>
2024-03-26 10:19:43 +0100

    OBJECT_HISTORY constant correction

--- bff-api/src/main/java/org/smartbit4all/bff/api/search/SearchPageApi.java
+++ bff-api/src/main/java/org/smartbit4all/bff/api/search/SearchPageApi.java
@@ -23,7 +23,7 @@ import org.smartbit4all.bff.api.searchpage.bean.SearchPageModel;
  */
 public interface SearchPageApi extends PageApi<SearchPageModel> {
 
-  static final String OBJECT_HISTOTY = "objectHistory";
+  static final String OBJECT_HISTORY = "objectHistory";
 
   /**
    * The generic query action that will run the execute search with the current filter expressions.
