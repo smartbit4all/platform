edc0c8a387d2af9ef04066ae1a0cb48e9ceb8fb7
AdamOtvos <adam.otvos@it4all.hu>
2024-04-04 14:37:28 +0200

    [#16918] add comment to well-known api

--- api/src/main/java/org/smartbit4all/api/wellknown/WellknownApi.java
+++ api/src/main/java/org/smartbit4all/api/wellknown/WellknownApi.java
@@ -8,5 +8,10 @@ public interface WellknownApi {
   public static final String VALUE_SET_WELLKNOWN = "valueSetWellknown";
   public static final String SL_WELLKNOWN = "storedListSetWellknown";
 
+  /**
+   * Based on the identifier, it returns the object node, which contains the appropriate data for
+   * verification
+   */
+
   ObjectNode getWellknownById(String id);
 }
