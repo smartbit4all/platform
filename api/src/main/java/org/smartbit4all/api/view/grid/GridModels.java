4a347621d17ae1cf0b04787d423593ca9155daa2
Attila Mate <attila.mate@it4all.hu>
2024-04-05 18:43:34 +0200

    [#14625] add setValueFromGridRow to GridModels

--- api/src/main/java/org/smartbit4all/api/view/grid/GridModels.java
+++ api/src/main/java/org/smartbit4all/api/view/grid/GridModels.java
@@ -47,6 +47,13 @@ public class GridModels {
         : null;
   }
 
+  public static final void setValueFromGridRow(GridRow gridRow, String property, Object value) {
+    Objects.requireNonNull(gridRow);
+    if (gridRow.getData() instanceof Map) {
+      ((Map<String, Object>) gridRow.getData()).put(property, value);
+    }
+  }
+
   public static void hideColumns(GridModel grid, String... columns) {
     hideColumns(grid, Arrays.asList(columns));
   }
