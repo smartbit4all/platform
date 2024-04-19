c739024acaec1d815192175416bee3ec2b629dc8
Zsolt Lipcsei <zsolt.lipcsei@it4all.hu>
2024-04-19 18:40:41 +0200

    [#14969] overload calculateActions method

--- api/src/main/java/org/smartbit4all/api/view/ActionManagementApi.java
+++ api/src/main/java/org/smartbit4all/api/view/ActionManagementApi.java
@@ -23,4 +23,16 @@ public interface ActionManagementApi extends PrimaryApi<ActionSupplierApi> {
    */
   List<UiAction> calculateActions(Object item, String... menus);
 
+  /**
+   * Calculates all the actions available on the given item. It is based on the registered
+   * {@link ActionSupplierApi}s and also on the context based on the item itself and the view the
+   * given calculation is applied on. The actions are a hierarchical structure representing a menus
+   * and sub menus.
+   * 
+   * @param item The object the action are calculated for.
+   * @param menus The list of menu actions to calculate.
+   * @return
+   */
+  List<UiAction> calculateActions(Object item, List<String> menus);
+
 }
--- api/src/main/java/org/smartbit4all/api/view/ActionManagementApiImpl.java
+++ api/src/main/java/org/smartbit4all/api/view/ActionManagementApiImpl.java
@@ -1,12 +1,13 @@
 package org.smartbit4all.api.view;
 
+import static java.util.stream.Collectors.toList;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import org.smartbit4all.api.contribution.PrimaryApiImpl;
 import org.smartbit4all.api.view.bean.UiAction;
-import static java.util.stream.Collectors.toList;
 
 public class ActionManagementApiImpl extends PrimaryApiImpl<ActionSupplierApi>
     implements ActionManagementApi {
@@ -17,16 +18,22 @@ public class ActionManagementApiImpl extends PrimaryApiImpl<ActionSupplierApi>
 
   @Override
   public List<UiAction> calculateActions(Object item, String... menus) {
-    List<UiAction> result = new ArrayList<>();
     List<String> menuList = menus == null ? Collections.emptyList() : Arrays.asList(menus);
+    return calculateActions(item, menuList);
+  }
+
+  @Override
+  public List<UiAction> calculateActions(Object item, List<String> menus) {
+    Objects.nonNull(menus);
+    List<UiAction> result = new ArrayList<>();
     List<ActionSupplierApi> extendingApis =
-        getContributionApis().values().stream().filter(a -> a.extending(item, menuList))
+        getContributionApis().values().stream().filter(a -> a.extending(item, menus))
             .collect(toList());
     for (ActionSupplierApi api : extendingApis) {
-      result = api.buildActions(item, menuList, result);
+      result = api.buildActions(item, menus, result);
     }
     for (ActionSupplierApi api : extendingApis) {
-      result = api.postProcessActions(item, menuList, result);
+      result = api.postProcessActions(item, menus, result);
     }
     // TODO Now apply the ACL by knowing the ACL conditions of the UiActions.
     return result;
