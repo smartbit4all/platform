3fc0e83a908b1d5ecbe618745ce49a8484d3e7ee
Gergely Ujvári <gergely.ujvari@it4all.hu>
2024-04-29 16:23:58 +0200

    [#15858] validateMandatoryFields returns distinct elements

--- api/src/main/java/org/smartbit4all/api/object/ObjectValidations.java
+++ api/src/main/java/org/smartbit4all/api/object/ObjectValidations.java
@@ -354,7 +354,7 @@ public final class ObjectValidations {
             && isTrue(it.getMandatory()))
         .map(ComponentConstraint::getDataName)
         .filter(widgetKeyPredicate)
-        .map(it -> it.split("\\."))
+        .map(it -> it.split("\\.")).distinct()
         .collect(toList());
     if (mandatoryProperties.isEmpty()) {
       return OK();
