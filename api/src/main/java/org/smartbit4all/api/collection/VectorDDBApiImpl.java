de3368012000e22790288e422d7232545431847d
Tóth Szabolcs Tamás <szabolcs.toth@it4all.hu>
2024-08-08 13:39:32 +0200

    [#18951] log added

--- api/src/main/java/org/smartbit4all/api/collection/VectorDDBApiImpl.java
+++ api/src/main/java/org/smartbit4all/api/collection/VectorDDBApiImpl.java
@@ -74,6 +74,8 @@ public final class VectorDDBApiImpl extends PrimaryApiImpl<VectorDBContibutionAp
   @Override
   public boolean collectionExists(ServiceConnection dbConnection, String name) {
     log.debug(getContributionApis().toString());
+    log.debug(name);
+    log.debug(dbConnection.toString());
     Objects.requireNonNull(dbConnection);
     VectorDBContibutionApi contributionApi = getContributionApi(dbConnection.getApiName());
     Objects.requireNonNull(contributionApi);
