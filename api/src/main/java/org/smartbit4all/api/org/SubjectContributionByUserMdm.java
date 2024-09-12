886f536ae21de66b353f4ff61d209cf4f0007b14
Zoltán Suller <zoltan.suller@it4all.hu>
2024-09-12 15:24:16 +0200

    [#19110] Mdm user subject contribution api implementation

--- api/src/main/java/org/smartbit4all/api/org/SubjectContributionByUserMdm.java
+++ api/src/main/java/org/smartbit4all/api/org/SubjectContributionByUserMdm.java
@@ -4,52 +4,87 @@ import static java.util.stream.Collectors.toList;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.smartbit4all.api.collection.StoredList;
 import org.smartbit4all.api.contribution.ContributionApiImpl;
+import org.smartbit4all.api.mdm.MDMEntryApi;
+import org.smartbit4all.api.mdm.MasterDataManagementApi;
 import org.smartbit4all.api.org.bean.Subject;
 import org.smartbit4all.api.org.bean.User;
 import org.smartbit4all.core.object.ObjectApi;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class SubjectContributionByUser extends ContributionApiImpl
+public class SubjectContributionByUserMdm extends ContributionApiImpl
     implements SubjectContributionApi {
 
-  @Autowired(required = false)
-  private OrgApi orgApi;
-
-  @Autowired(required = false)
+  @Autowired
   private ObjectApi objectApi;
 
-  public SubjectContributionByUser() {
-    super(SubjectContributionByUser.class.getName());
+  @Autowired
+  private MasterDataManagementApi masterDataManagementApi;
+
+  private Long lastCacheRerfeshTime = null;
+
+  private List<Subject> allSubjects;
+
+  private String definitionName;
+
+  private String entryName;
+
+  private Map<URI, User> usersByUri;
+
+  private Map<String, User> usersById;
+
+  public SubjectContributionByUserMdm(String apiName, String definitionName, String entryName) {
+    super(apiName);
+    this.definitionName = definitionName;
+    this.entryName = entryName;
+  }
+
+  private final void refreshCache(String modelName) {
+    MDMEntryApi mdmUserApi = masterDataManagementApi.getApi(definitionName, entryName);
+    StoredList userList = mdmUserApi.getList();
+    if (lastCacheRerfeshTime == null
+        || (userList.exists() && !userList.getLastModified().equals(lastCacheRerfeshTime))) {
+      allSubjects = userList.nodesFromCache()
+          .map(n -> new Subject()
+              .model(modelName)
+              .type(User.class.getName())
+              .ref(n.getObjectUri()))
+          .collect(toList());
+
+      usersByUri = new HashMap<>();
+      usersById = new HashMap<>();
+      userList.nodesFromCache().forEach(node -> {
+        User user = node.getObject(User.class);
+        usersByUri.put(objectApi.getLatestUri(node.getObjectUri()), user);
+        usersById.put(user.getUsername(), user);
+      });
+      lastCacheRerfeshTime = userList.getLastModified();
+    }
   }
 
   @Override
   public List<Subject> getUserSubjects(String modelName, URI userUri) {
-    if (orgApi == null) {
-      return Collections.emptyList();
-    }
+    refreshCache(modelName);
     List<Subject> result = new ArrayList<>();
-    if (orgApi.getUser(userUri) != null) {
-      result.add(new Subject()
-          .model(modelName)
-          .type(User.class.getName())
-          .ref(userUri));
+    User user = usersByUri == null ? null : usersByUri.get(userUri);
+    if (user != null) {
+      result.add(
+          new Subject()
+              .model(modelName)
+              .type(User.class.getName())
+              .ref(userUri));
     }
     return result;
   }
 
   @Override
   public List<Subject> getAllSubjects(String modelName) {
-    if (orgApi == null) {
-      return Collections.emptyList();
-    }
-    return orgApi.getAllUsers().stream()
-        .map(u -> new Subject()
-            .model(modelName)
-            .type(User.class.getName())
-            .ref(u.getUri()))
-        .collect(toList());
+    refreshCache(modelName);
+    return new ArrayList<>(allSubjects);
   }
 
   /**
@@ -57,12 +92,13 @@ public class SubjectContributionByUser extends ContributionApiImpl
    */
   @Override
   public List<URI> getUsersOf(String modelName, List<URI> subjects) {
+    refreshCache(modelName);
     if (subjects == null || objectApi == null) {
       return Collections.emptyList();
     }
     return subjects.stream()
         .filter(u -> objectApi.definition(u).instanceOf(User.class))
-        .filter(u -> orgApi.getUser(u) != null)
+        .filter(u -> usersByUri.containsKey(u))
         .map(u -> objectApi.getLatestUri(u))
         .collect(toList());
   }
@@ -81,11 +117,12 @@ public class SubjectContributionByUser extends ContributionApiImpl
     if (baseList == null || objectApi == null) {
       return Collections.emptyList();
     }
+    refreshCache(modelName);
     // Here we add all the parent organizations because we are part of it. If we need to have
     // another approach then we need to register this contribution on other name.
     return baseList.stream()
         .filter(s -> objectApi.definition(s).instanceOf(User.class))
-        .filter(u -> orgApi.getUser(u) != null)
+        .filter(u -> usersByUri.containsKey(u))
         .map(e -> new Subject()
             .model(modelName)
             .type(User.class.getName())
@@ -95,9 +132,10 @@ public class SubjectContributionByUser extends ContributionApiImpl
 
   @Override
   public List<String> getDisplayValue(String modelName, List<URI> subjects) {
+    refreshCache(modelName);
     return subjects.stream()
         .filter(s -> objectApi.definition(s).instanceOf(User.class))
-        .filter(u -> orgApi.getUser(u) != null)
+        .filter(u -> usersByUri.containsKey(u))
         .map(objectApi::loadLatest)
         .map(n -> n.getValueAsString(User.NAME))
         .collect(toList());
