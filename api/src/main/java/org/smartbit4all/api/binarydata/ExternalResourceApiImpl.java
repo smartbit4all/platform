7b60c828cf73555fb055517ca49b5627c1ba01c5
Tóth Szabolcs Tamás <szabolcs.toth@it4all.hu>
2024-10-16 12:33:06 +0200

    [#18740] temp solution to pipeline

--- api/src/main/java/org/smartbit4all/api/binarydata/ExternalResourceApiImpl.java
+++ api/src/main/java/org/smartbit4all/api/binarydata/ExternalResourceApiImpl.java
@@ -1,17 +1,10 @@
 package org.smartbit4all.api.binarydata;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.smartbit4all.api.attachment.bean.BinaryContentData;
 import org.smartbit4all.api.mimetype.MimeTypeApi;
 import org.smartbit4all.api.session.SessionApi;
-import org.smartbit4all.api.session.bean.UserActivityLog;
 import org.smartbit4all.core.object.ObjectApi;
-import org.smartbit4all.core.utility.StringConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ExternalResourceApiImpl implements ExternalResourceApi {
@@ -26,59 +19,64 @@ public class ExternalResourceApiImpl implements ExternalResourceApi {
   private MimeTypeApi mimeTypeApi;
 
   @Override
-  public BinaryContentData downloadFile(String schema, URI uri) {
-    HttpURLConnection connection = null;
-
-    try {
-      URL url = uri.toURL();
-      connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("GET");
-
-      int responseCode = connection.getResponseCode();
-
-      if (responseCode == HttpURLConnection.HTTP_OK) {
-        String filename = StringConstant.EMPTY;
-        String fileExtension = StringConstant.EMPTY;
-        String mimeType = StringConstant.EMPTY;
-
-        String contentDisposition = connection.getHeaderField("Content-Disposition");
-        if (contentDisposition != null && contentDisposition.contains("filename=")) {
-          Pattern pattern = Pattern.compile("filename=\"?([^\";]*)\"?");
-          Matcher matcher = pattern.matcher(contentDisposition);
-          if (matcher.find()) {
-            filename = matcher.group(1);
-            fileExtension = mimeTypeApi.getExtensionFromFileName(filename);
-            mimeType = mimeTypeApi.getMimeType(filename);
-          }
-        }
-
-        byte[] allBytes = connection.getInputStream().readAllBytes();
-
-        UserActivityLog created = sessionApi.createActivityLog();
-        BinaryData binaryData = new BinaryData(allBytes);
-
-        return new BinaryContentData()
-            .fileName(filename)
-            .mimeType(mimeType)
-            .size(Integer.toUnsignedLong(allBytes.length))
-            .created(created)
-            .updated(created)
-            .extension(fileExtension)
-            .mimeType(mimeType)
-            .contentHash(binaryData.hashIfPresent())
-            .dataUri(objectApi.saveAsNew(schema, new BinaryDataObject(binaryData)));
-      } else {
-        throw new IllegalStateException(
-            "Downloading file is unsuccessful, response is: " + responseCode);
-      }
-    } catch (IOException e) {
-      throw new IllegalStateException("Downloading file is unsuccessful", e);
-    } finally {
-      if (connection != null) {
-        connection.disconnect();
-      }
-    }
+  public BinaryContentData downloadFile(String schema, URI url) {
+    return new BinaryContentData().fileName("temp");
   }
+
+  // @Override
+  // public BinaryContentData downloadFile(String schema, URI uri) {
+  // HttpURLConnection connection = null;
+  //
+  // try {
+  // URL url = uri.toURL();
+  // connection = (HttpURLConnection) url.openConnection();
+  // connection.setRequestMethod("GET");
+  //
+  // int responseCode = connection.getResponseCode();
+  //
+  // if (responseCode == HttpURLConnection.HTTP_OK) {
+  // String filename = StringConstant.EMPTY;
+  // String fileExtension = StringConstant.EMPTY;
+  // String mimeType = StringConstant.EMPTY;
+  //
+  // String contentDisposition = connection.getHeaderField("Content-Disposition");
+  // if (contentDisposition != null && contentDisposition.contains("filename=")) {
+  // Pattern pattern = Pattern.compile("filename=\"?([^\";]*)\"?");
+  // Matcher matcher = pattern.matcher(contentDisposition);
+  // if (matcher.find()) {
+  // filename = matcher.group(1);
+  // fileExtension = mimeTypeApi.getExtensionFromFileName(filename);
+  // mimeType = mimeTypeApi.getMimeType(filename);
+  // }
+  // }
+  //
+  // byte[] allBytes = connection.getInputStream().readAllBytes();
+  //
+  // UserActivityLog created = sessionApi.createActivityLog();
+  // BinaryData binaryData = new BinaryData(allBytes);
+  //
+  // return new BinaryContentData()
+  // .fileName(filename)
+  // .mimeType(mimeType)
+  // .size(Integer.toUnsignedLong(allBytes.length))
+  // .created(created)
+  // .updated(created)
+  // .extension(fileExtension)
+  // .mimeType(mimeType)
+  // .contentHash(binaryData.hashIfPresent())
+  // .dataUri(objectApi.saveAsNew(schema, new BinaryDataObject(binaryData)));
+  // } else {
+  // throw new IllegalStateException(
+  // "Downloading file is unsuccessful, response is: " + responseCode);
+  // }
+  // } catch (IOException e) {
+  // throw new IllegalStateException("Downloading file is unsuccessful", e);
+  // } finally {
+  // if (connection != null) {
+  // connection.disconnect();
+  // }
+  // }
+  // }
 
   // @Override
   // public BinaryContentData downloadFile(String schema, URI url) {
