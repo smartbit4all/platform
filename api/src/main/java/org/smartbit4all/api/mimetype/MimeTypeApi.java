ce2a2409a4fcd3d9411ae96b89807afad5245bcd
Tóth Szabolcs Tamás <szabolcs.toth@it4all.hu>
2024-10-28 10:23:50 +0100

    [#18899] mime type api updates

--- api/src/main/java/org/smartbit4all/api/mimetype/MimeTypeApi.java
+++ api/src/main/java/org/smartbit4all/api/mimetype/MimeTypeApi.java
@@ -39,24 +39,110 @@ public class MimeTypeApi implements InitializingBean {
 
   public static final String ZIP_MIMETYPE = MediaType.ZIP.toString();
 
+  public static final String JSON_MIMETYPE = MediaType.JSON_UTF_8.toString();
+  public static final String JAVASCRIPT_MIMETYPE = MediaType.JAVASCRIPT_UTF_8.toString();
+  public static final String XML_MIMETYPE = MediaType.XML_UTF_8.toString();
+  public static final String URL_ENCODED_MIMETYPE = "application/x-www-form-urlencoded";
+  public static final String CSS_MIMETYPE = MediaType.CSS_UTF_8.toString();
+  public static final String CSV_MIMETYPE = "text/csv";
+  public static final String WEBP_MIMETYPE = "image/webp";
+  public static final String BMP_MIMETYPE = "image/bmp";
+  public static final String SVG_MIMETYPE = "image/svg+xml";
+  public static final String TIFF_MIMETYPE = "image/tiff";
+  public static final String ICON_MIMETYPE = "image/x-icon";
+  public static final String MP4_MIMETYPE = "video/mp4";
+  public static final String MPEG_MIMETYPE = "video/mpeg";
+  public static final String WEBM_VIDEO_MIMETYPE = "video/webm";
+  public static final String OGG_VIDEO_MIMETYPE = "video/ogg";
+  public static final String MP3_MIMETYPE = MediaType.MPEG_AUDIO.toString();
+  public static final String OGG_AUDIO_MIMETYPE = "audio/ogg";
+  public static final String WAV_MIMETYPE = "audio/wav";
+  public static final String WEBM_AUDIO_MIMETYPE = "audio/webm";
+  public static final String GZIP_MIMETYPE = "application/gzip";
+  public static final String EXCEL_MIMETYPE = "application/vnd.ms-excel";
+  public static final String EXCEL_XLSX_MIMETYPE =
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+  public static final String WORD_MIMETYPE = "application/msword";
+  public static final String WORD_DOCX_MIMETYPE =
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+  public static final String POWERPOINT_MIMETYPE = "application/vnd.ms-powerpoint";
+  public static final String POWERPOINT_PPTX_MIMETYPE =
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+  public static final String TAR_MIMETYPE = "application/x-tar";
+  public static final String SEVEN_ZIP_MIMETYPE = "application/x-7z-compressed";
+  public static final String RAR_MIMETYPE = "application/x-rar-compressed";
+  public static final String FLASH_MIMETYPE = "application/x-shockwave-flash";
+  public static final String BINARY_MIMETYPE = MediaType.OCTET_STREAM.toString();
+  public static final String JSON_API_MIMETYPE = "application/vnd.api+json";
+  public static final String XUL_MIMETYPE = "application/vnd.mozilla.xul+xml";
+  public static final String MULTIPART_FORM_MIMETYPE = MediaType.FORM_DATA.toString();
+  public static final String EPUB_MIMETYPE = "application/epub+zip";
+  public static final String TTF_MIMETYPE = "font/ttf";
+  public static final String WOFF_MIMETYPE = "font/woff";
+  public static final String WOFF2_MIMETYPE = "font/woff2";
+  public static final String ODT_MIMETYPE = "application/vnd.oasis.opendocument.text";
+  public static final String ODS_MIMETYPE = "application/vnd.oasis.opendocument.spreadsheet";
+  public static final String ODP_MIMETYPE = "application/vnd.oasis.opendocument.presentation";
+  public static final String GLTF_MIMETYPE = "model/gltf+json";
+
+  // extensions
   public static final String PDF_EXT = "pdf";
   public static final String DOCX_EXT = "docx";
   public static final String DOC_EXT = "doc";
-
   public static final String TXT_EXT = "txt";
   public static final String HTML_EXT = "html";
-
   public static final String PNG_EXT = "png";
   public static final String JPEG_EXT = "jpg";
   public static final String GIF_EXT = "gif";
-
   public static final String XLSX_EXT = "xlsx";
   public static final String XLS_EXT = "xls";
-
   public static final String PPT_EXT = "ppt";
   public static final String PPTX_EXT = "pptx";
-
   public static final String ZIP_EXT = "zip";
+  public static final String PLAIN_EXT = "txt";
+  public static final String JSON_EXT = "json";
+  public static final String JAVASCRIPT_EXT = "js";
+  public static final String XML_EXT = "xml";
+  public static final String URL_ENCODED_EXT = "urlencoded";
+  public static final String CSS_EXT = "css";
+  public static final String CSV_EXT = "csv";
+  public static final String WEBP_EXT = "webp";
+  public static final String BMP_EXT = "bmp";
+  public static final String SVG_EXT = "svg";
+  public static final String TIFF_EXT = "tiff";
+  public static final String ICON_EXT = "ico";
+  public static final String MP4_EXT = "mp4";
+  public static final String MPEG_EXT = "mpeg";
+  public static final String WEBM_VIDEO_EXT = "webm";
+  public static final String OGG_VIDEO_EXT = "ogv";
+  public static final String MP3_EXT = "mp3";
+  public static final String OGG_AUDIO_EXT = "oga";
+  public static final String WAV_EXT = "wav";
+  public static final String WEBM_AUDIO_EXT = "weba";
+  public static final String GZIP_EXT = "gz";
+  public static final String EXCEL_EXT = "xls";
+  public static final String EXCEL_XLSX_EXT = "xlsx";
+  public static final String WORD_EXT = "doc";
+  public static final String WORD_DOCX_EXT = "docx";
+  public static final String POWERPOINT_EXT = "ppt";
+  public static final String POWERPOINT_PPTX_EXT = "pptx";
+  public static final String TAR_EXT = "tar";
+  public static final String SEVEN_ZIP_EXT = "7z";
+  public static final String RAR_EXT = "rar";
+  public static final String FLASH_EXT = "swf";
+  public static final String BINARY_EXT = "bin";
+  public static final String JSON_API_EXT = "json";
+  public static final String XUL_EXT = "xul";
+  public static final String MULTIPART_FORM_EXT = "multipart";
+  public static final String EPUB_EXT = "epub";
+  public static final String TTF_EXT = "ttf";
+  public static final String WOFF_EXT = "woff";
+  public static final String WOFF2_EXT = "woff2";
+  public static final String ODT_EXT = "odt";
+  public static final String ODS_EXT = "ods";
+  public static final String ODP_EXT = "odp";
+  public static final String GLTF_EXT = "gltf";
+
 
 
   /**
