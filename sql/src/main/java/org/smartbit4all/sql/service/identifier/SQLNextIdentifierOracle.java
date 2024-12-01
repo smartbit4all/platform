de1aca94bfc56d33c27e786c6c4f4adecc41ffdc
Attila Mate <attila.mate@it4all.hu>
2024-12-01 10:52:41 +0100

    [#20710] don't show sequence doesn't exist exception
    It will be created after exception, it's just polluting the log.
    Creation exception will be logged.

--- sql/src/main/java/org/smartbit4all/sql/service/identifier/SQLNextIdentifierOracle.java
+++ sql/src/main/java/org/smartbit4all/sql/service/identifier/SQLNextIdentifierOracle.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
  * Copyright (C) 2020 - 2020 it4all Hungary Kft.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -39,7 +39,7 @@ public class SQLNextIdentifierOracle extends SB4FunctionImpl<String, Long>
       nextVal();
     } catch (Exception e) {
       // Try to create the sequence
-      log.warn("Unable to find " + input + " sequence try to create it.", e);
+      log.warn("Unable to find " + input + " sequence, trying to create it.");
       try {
         jdbcTemplate.execute("create sequence " + input);
       } catch (Exception e1) {
