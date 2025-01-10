b3c22683d82d24091e5551ecfafae696573883a3
Zsolt Lipcsei <zsolt.lipcsei@it4all.hu>
2025-01-10 16:47:13 +0100

    [#21554] add logging interceptor

---
+++ api-cli-rest/src/main/java/org/smartbit4all/api/interceptor/LoggingInterceptor.java
@@ -0,0 +1,33 @@
+package org.smartbit4all.api.interceptor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+public class LoggingInterceptor implements ClientHttpRequestInterceptor {
+
+  private static Logger LOGGER = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+  @Override
+  public ClientHttpResponse intercept(
+      HttpRequest req, byte[] reqBody, ClientHttpRequestExecution ex) throws IOException {
+    LOGGER.info("Request path: {}, request body: {}", req.getURI(),
+        new String(reqBody, StandardCharsets.UTF_8));
+    ClientHttpResponse response = ex.execute(req, reqBody);
+    InputStreamReader isr = new InputStreamReader(
+        response.getBody(), StandardCharsets.UTF_8);
+    String body = new BufferedReader(isr).lines()
+        .collect(Collectors.joining("\n"));
+    LOGGER.info("Response for {} is [status: {}, body: {}]", req.getURI(),
+        response.getStatusCode().value(), body);
+    return response;
+  }
+}
