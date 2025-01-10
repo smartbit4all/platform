94d1a0cf6e9c578cb5c7e2db09fb4e638aa6ae6c
Tóth Szabolcs Tamás <szabolcs.toth@it4all.hu>
2025-01-10 13:35:39 +0100

    [#21577] GPSPositions constants added

---
+++ api/src/main/java/org/smartbit4all/api/view/geomap/GPSPositions.java
@@ -0,0 +1,13 @@
+package org.smartbit4all.api.view.geomap;
+
+import org.smartbit4all.api.geomap.bean.GPSPosition;
+
+public class GPSPositions {
+
+
+  public static final GPSPosition BUDAPEST = new GPSPosition()
+      .latitude(47.4979f)
+      .longitude(19.0402f);
+
+  private GPSPositions() {}
+}
