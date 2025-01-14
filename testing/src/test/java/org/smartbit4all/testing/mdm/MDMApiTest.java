409f759bf2e53a2a7b48635807881aacfb79b6df
Szabolcs Bazil Papp <szabolcs.papp@it4all.hu>
2025-01-14 15:36:25 +0100

    [#misc] declare application runtime and invocation registry related refresh job frequency properties as small values for tests relying on them having run previously

--- testing/src/test/java/org/smartbit4all/testing/mdm/MDMApiTest.java
+++ testing/src/test/java/org/smartbit4all/testing/mdm/MDMApiTest.java
@@ -107,7 +107,10 @@ import org.smartbit4all.testing.UITestApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {MDMApiTestConfig.class})
+@SpringBootTest(classes = {MDMApiTestConfig.class}, properties = {
+    "invocationregistry.refresh.fixeddelay=2000",
+    "applicationruntime.maintain.fixeddelay=2000"
+})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(Lifecycle.PER_CLASS)
 class MDMApiTest {
