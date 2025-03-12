11073062cf908f696a1c5341f18580d71cf0769d
Tóth Szabolcs Tamás <szabolcs.toth@it4all.hu>
2025-03-12 14:37:37 +0100

    [#22679] if debug enabled, then we log the ffmpeg isAvailable output

--- api/src/main/java/org/smartbit4all/api/commandexecutor/CommandExecutorFfmpegApi.java
+++ api/src/main/java/org/smartbit4all/api/commandexecutor/CommandExecutorFfmpegApi.java
@@ -58,6 +58,9 @@ public class CommandExecutorFfmpegApi extends CommandExecutorApiAbs implements C
     try (InputStream in = process.getInputStream()) {
       byte[] allBytes = FileIO.readInputStreamToByteArray(in);
       String outputString = new String(allBytes);
+      if (log.isDebugEnabled()) {
+        log.debug(outputString);
+      }
       return outputString.contains("FFmpeg");
     } catch (IOException e) {
       log.error(e.getMessage(), e);
