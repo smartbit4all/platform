671051ad146bfe5e1d3027d29bd636d4dbf6e21e
Attila Mate <attila.mate@it4all.hu>
2025-05-16 13:20:02 +0200

    [#23660] add option to not show technical errors on ViewApi calls

--- api-srv-rest/src/main/java/org/smartbit4all/api/view/restserver/impl/ViewApiExceptionHandler.java
+++ api-srv-rest/src/main/java/org/smartbit4all/api/view/restserver/impl/ViewApiExceptionHandler.java
@@ -1,8 +1,14 @@
 package org.smartbit4all.api.view.restserver.impl;
 
+import java.text.MessageFormat;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smartbit4all.api.invocation.exception.BusinessLogicException;
+import org.smartbit4all.api.setting.LocaleSettingApi;
 import org.smartbit4all.api.view.bean.ViewApiError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,11 +17,29 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 // @ControllerAdvice(assignableTypes = {ViewApiMarker.class})
 public class ViewApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+  @Value("${view.exception.hide-technical-exceptions:false}")
+  protected boolean hideTechnicalExceptions;
+
+  @Autowired
+  protected LocaleSettingApi localeSettingApi;
+
   private static final Logger log = LoggerFactory.getLogger(ViewApiExceptionHandler.class);
 
   @ExceptionHandler(RuntimeException.class)
   protected ResponseEntity<Object> handleViewApiException(RuntimeException ex) {
-    log.warn("Handling runtime error", ex);
+    if (hideTechnicalExceptions) {
+      if (!isBusinessLogicException(ex)) {
+        UUID errorId = UUID.randomUUID();
+        log.error("Technical error " + errorId, ex);
+        String message = localeSettingApi.get("view.exception.technical");
+        message = MessageFormat.format(
+            message, errorId);
+        return handleViewApiException(message);
+      }
+      log.trace(ex.getMessage(), ex);
+    } else {
+      log.warn("Handling runtime error", ex);
+    }
     return handleViewApiException(ex.getMessage());
   }
 
@@ -25,4 +49,10 @@ public class ViewApiExceptionHandler extends ResponseEntityExceptionHandler {
             .message(message));
   }
 
+  protected boolean isBusinessLogicException(RuntimeException ex) {
+    return ex instanceof BusinessLogicException
+        || (ex instanceof NullPointerException
+            && ex.getMessage() != null
+            && !ex.getMessage().endsWith(" is null"));
+  }
 }
