a2745229e2282490196d98919951dd5dc05d97a5
Attila Mate <attila.mate@it4all.hu>
2025-05-16 17:24:21 +0200

    [#18123] don't return null filter model in rest API

--- api-srv-rest/src/main/java/org/smartbit4all/api/filterexpression/restserver/impl/FilterApiDelegateImpl.java
+++ api-srv-rest/src/main/java/org/smartbit4all/api/filterexpression/restserver/impl/FilterApiDelegateImpl.java
@@ -1,7 +1,10 @@
 package org.smartbit4all.api.filterexpression.restserver.impl;
 
 import java.util.UUID;
+import org.smartbit4all.api.filterexpression.bean.FilterExpressionBuilderModel;
 import org.smartbit4all.api.filterexpression.bean.FilterExpressionBuilderUiModel;
+import org.smartbit4all.api.filterexpression.bean.FilterExpressionBuilderUiModel.TypeEnum;
+import org.smartbit4all.api.filterexpression.bean.FilterExpressionFieldList;
 import org.smartbit4all.api.filterexpression.restserver.FilterApiDelegate;
 import org.smartbit4all.api.view.bean.UiActionRequest;
 import org.smartbit4all.api.view.filterexpression.FilterExpressionBuilderApi;
@@ -17,9 +20,15 @@ public class FilterApiDelegateImpl implements FilterApiDelegate {
   @Override
   public ResponseEntity<FilterExpressionBuilderUiModel> load(UUID uuid, String filterIdentifier)
       throws Exception {
-    return ResponseEntity.ok(filterExpressionBuilderApi.load(uuid, filterIdentifier));
+    FilterExpressionBuilderUiModel model = filterExpressionBuilderApi.load(uuid, filterIdentifier);
+    if (model == null) {
+      model = new FilterExpressionBuilderUiModel()
+          .type(TypeEnum.SIMPLE)
+          .model(new FilterExpressionBuilderModel()
+              .workplaceList(new FilterExpressionFieldList()));
+    }
+    return ResponseEntity.ok(model);
   }
-
 
   @Override
   public ResponseEntity<FilterExpressionBuilderUiModel> performWidgetAction(UUID uuid,
