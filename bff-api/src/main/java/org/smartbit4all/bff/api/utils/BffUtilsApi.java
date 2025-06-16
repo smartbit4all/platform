39ff8ed669b2e3a09e8baea8159aff2a8738179f
Pogány Benedek Márk <benedek.pogany@it4all.hu>
2025-06-16 11:08:40 +0200

    [#24071] change ServiceConnectionEditor
    Change ServiceConnectionEditor grid type to GenericValue from KeyPairValue, as paginator crashed from object deserialization

--- bff-api/src/main/java/org/smartbit4all/bff/api/utils/BffUtilsApi.java
+++ bff-api/src/main/java/org/smartbit4all/bff/api/utils/BffUtilsApi.java
@@ -2,11 +2,15 @@ package org.smartbit4all.bff.api.utils;
 
 import java.util.UUID;
 import org.smartbit4all.api.invocation.bean.InvocationRequest;
+import org.smartbit4all.api.value.bean.GenericValue;
 import org.smartbit4all.api.value.bean.KeyValuePair;
 
 public interface BffUtilsApi {
 
   void showMapEntryEditor(UUID viewUuid, String gridId, KeyValuePair pageModel,
+      boolean keyEditable, InvocationRequest saveRequest);
+
+  void showMapEntryEditor(UUID viewUuid, String gridId, GenericValue pageModel,
       boolean keyEditable, InvocationRequest saveRequest);
 
   <T> T getValueFromGridRow(UUID viewUuid, String widgetId, String nodeId, String key,
