1b05f79909d60b29c54ff3f6e03ed58ef98a55ce
Szabolcs Bazil Papp <szabolcs.papp@it4all.hu>
2025-08-04 11:04:46 +0200

    [#24710] use virtual threads to batch load on StorageFS

--- api/src/main/java/org/smartbit4all/domain/data/storage/StorageConfigurationProperties.java
+++ api/src/main/java/org/smartbit4all/domain/data/storage/StorageConfigurationProperties.java
@@ -4,5 +4,11 @@ import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("sb4-storage")
-public record StorageConfigurationProperties(Map<String, String> schemaAlias) {
+public record StorageConfigurationProperties(
+    Map<String, String> schemaAlias,
+    StorageConfigurationProperties.FileSystem fs) {
+
+  public record FileSystem(int parallelLoadThreshold) {
+  }
+
 }
