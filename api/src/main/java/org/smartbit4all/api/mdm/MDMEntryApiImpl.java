7560882935c7cfaafe8b9b9927301810bdfdd78f
Zsolt Lipcsei <zsolt.lipcsei@it4all.hu>
2025-09-25 14:30:29 +0200

    Revert "[#24836] save mdm object even if it presents as self contained object"
    This reverts commit 46cbbd908c874909c469faabbb62500fe93cb467.

--- api/src/main/java/org/smartbit4all/api/mdm/MDMEntryApiImpl.java
+++ api/src/main/java/org/smartbit4all/api/mdm/MDMEntryApiImpl.java
@@ -66,7 +66,6 @@ import org.smartbit4all.core.object.ObjectDefinition;
 import org.smartbit4all.core.object.ObjectNode;
 import org.smartbit4all.core.object.ObjectPropertyResolver;
 import org.smartbit4all.core.utility.StringConstant;
-import org.smartbit4all.domain.data.storage.ObjectStorageImpl;
 import org.springframework.util.ObjectUtils;
 
 /**
@@ -232,19 +231,7 @@ public final class MDMEntryApiImpl implements MDMEntryApi {
       }
 
       Map<URI, URI> savedUrisByLatest =
-          results.stream().collect(toMap(u -> objectApi.getLatestUri(u), u -> u, (u1, u2) -> {
-            Long uv1 = ObjectStorageImpl.getUriVersion(u1);
-            Long uv2 = ObjectStorageImpl.getUriVersion(u2);
-            if (uv1 == null) {
-              return u2;
-            } else if (uv2 == null) {
-              return u1;
-            } else if (uv1 > uv2) {
-              return u1;
-            } else {
-              return u2;
-            }
-          }));
+          results.stream().collect(toMap(u -> objectApi.getLatestUri(u), u -> u));
 
       // Merge the existing ones
       List<URI> merged = l.stream().map(u -> {
