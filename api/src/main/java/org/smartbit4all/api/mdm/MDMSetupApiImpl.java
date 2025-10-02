e4d82d8b6d1c44a5bf0d9fd8b43ce106a1dc41c1
Tóth Szabolcs Tamás <szabolcs.toth@it4all.hu>
2025-10-02 17:49:05 +0200

    [#25516] setup modernisation

--- api/src/main/java/org/smartbit4all/api/mdm/MDMSetupApiImpl.java
+++ api/src/main/java/org/smartbit4all/api/mdm/MDMSetupApiImpl.java
@@ -1,13 +1,9 @@
 package org.smartbit4all.api.mdm;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
-import java.nio.charset.Charset;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -15,11 +11,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.IntStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smartbit4all.api.binarydata.BinaryData;
 import org.smartbit4all.api.collection.bean.VectorCollectionDescriptor;
-import org.smartbit4all.api.invocation.exception.BusinessLogicException;
 import org.smartbit4all.api.mdm.bean.MDMDefinition;
 import org.smartbit4all.api.mdm.bean.MDMEntryDescriptor;
 import org.smartbit4all.api.mdm.bean.MDMModificationRequest;
@@ -92,37 +88,39 @@ public class MDMSetupApiImpl implements MDMSetupApi {
     } catch (Exception e) {
       throw new IllegalArgumentException(e.getMessage(), e);
     }
-
     setupObject
         .stream()
         .filter(e -> Boolean.FALSE
-            .equals(mdmApi.entryDescriptorExists(e.getDefinition(), e.getCode(), null)))
+            .equals(mdmApi.entryDescriptorExists(e.getDefinition(), e.getCode(), null)) ||
+            mdmApi.getApi(e.getDefinition(), e.getCode()).getList().uris().isEmpty())
         .forEach(e -> {
-          MDMDefinition definition = mdmApi.getDefinition(e.getDefinition());
-          MDMDefinitionOption option = new MDMDefinitionOption(definition);
-          MDMEntryDescriptor descriptor =
-              option.addDefaultDescriptor(GenericValue.class, e.getCode())
-                  .tableColumns(
-                      e.getDisplayedColumns().stream()
-                          .map(s -> new MDMTableColumnDescriptor().name(s)
-                              .addPathItem(s))
-                          .toList())
-                  .displayNameForm(new LangString().defaultValue(e.getName()))
-                  .displayNameList(new LangString().defaultValue(e.getName()))
-                  .displayNamePropertyPath(Arrays.asList(GenericValue.NAME))
-                  .listPageGridViews(Collections.emptyList())
-                  .isValueSet(Boolean.TRUE)
-                  .vectorCollection(e.getVectorCollection() != null ? e.getVectorCollection()
-                      : new VectorCollectionDescriptor())
-                  .importable(Boolean.TRUE.equals(e.getImportable()))
-                  .csvSeparator(e.getCsvSeparator())
-                  .uniquePropertyPaths(Arrays.asList(Arrays.asList(GenericValue.CODE)));
-          MDMDefinitionOption.addCreatedUpdatedExtraProperties(descriptor);
-          // clear descriptors to not add already created descriptions again
-          option.getDefinition().getDescriptors().clear();
-          option.addDescriptor(descriptor);
-          mdmApi.addNewEntries(option, null);
-
+          if (Boolean.FALSE
+              .equals(mdmApi.entryDescriptorExists(e.getDefinition(), e.getCode(), null))) {
+            MDMDefinition definition = mdmApi.getDefinition(e.getDefinition());
+            MDMDefinitionOption option = new MDMDefinitionOption(definition);
+            MDMEntryDescriptor descriptor =
+                option.addDefaultDescriptor(GenericValue.class, e.getCode())
+                    .tableColumns(
+                        e.getDisplayedColumns().stream()
+                            .map(s -> new MDMTableColumnDescriptor().name(s)
+                                .addPathItem(s))
+                            .toList())
+                    .displayNameForm(new LangString().defaultValue(e.getName()))
+                    .displayNameList(new LangString().defaultValue(e.getName()))
+                    .displayNamePropertyPath(Arrays.asList(GenericValue.NAME))
+                    .listPageGridViews(Collections.emptyList())
+                    .isValueSet(Boolean.TRUE)
+                    .vectorCollection(e.getVectorCollection() != null ? e.getVectorCollection()
+                        : new VectorCollectionDescriptor())
+                    .importable(Boolean.TRUE.equals(e.getImportable()))
+                    .csvSeparator(e.getCsvSeparator())
+                    .uniquePropertyPaths(Arrays.asList(Arrays.asList(GenericValue.CODE)));
+            MDMDefinitionOption.addCreatedUpdatedExtraProperties(descriptor);
+            // clear descriptors to not add already created descriptions again
+            option.getDefinition().getDescriptors().clear();
+            option.addDescriptor(descriptor);
+            mdmApi.addNewEntries(option, null);
+          }
           if (!ObjectUtils.isEmpty(e.getFullCsvPath())) {
             try (InputStream is = new FileInputStream(e.getFullCsvPath())) {
               byte[] bytes = is.readAllBytes();
@@ -140,50 +138,51 @@ public class MDMSetupApiImpl implements MDMSetupApi {
   @Override
   public void importEntriesFromCsvFile(String definition, String entry, BinaryData csvFile,
       String csvSeparator, URI branchUri) {
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-        new BufferedInputStream(csvFile.inputStream()), Charset.defaultCharset()));
     String separator = ObjectUtils.isEmpty(csvSeparator) ? StringConstant.COMMA
         : csvSeparator;
     List<Map<String, String>> items = new ArrayList<>();
-    int index = 1;
-    try {
-      String line = bufferedReader.readLine();
-      List<String> keySet = Arrays.asList(line.split(separator));
-      line = bufferedReader.readLine();
-      while (line != null) {
-        String[] fields = line.split(separator);
-        Map<String, String> item = new HashMap<>();
-        for (int i = 0; i < fields.length; ++i) {
-          try {
-            item.put(keySet.get(i), fields[i]);
-          } catch (IndexOutOfBoundsException e) {
-            log.error("The {} line ({}) failed with the the following exception:", index, line);
-            log.error(e.getMessage(), e);
+    try (InputStream is = csvFile.inputStream()) {
+      byte[] allBytes = is.readAllBytes();
+      List<String> lines = Arrays.asList(new String(allBytes).split("\n"))
+          .stream()
+          .map(s -> s.replaceAll("\r", StringConstant.EMPTY))
+          .toList();
+      List<String> keySet = Arrays.asList(lines.get(0).split(separator));
+      if (lines.size() > 1) {
+        IntStream.range(1, lines.size()).forEach(i -> {
+          String line = lines.get(i);
+          String[] fields = line.split(separator);
+          Map<String, String> item = new HashMap<>();
+          for (int j = 0; j < keySet.size(); ++j) {
+            try {
+              item.put(keySet.get(j), fields[j]);
+            } catch (IndexOutOfBoundsException e) {
+              log.error("The {} line ({}) failed with the the following exception:", i, line);
+              log.error(e.getMessage(), e);
+            }
           }
-        }
-        items.add(item);
-        line = bufferedReader.readLine();
-        ++index;
+          items.add(item);
+        });
+      } else {
+        log.error("The csv file only contained the header");
+        return;
       }
-      MDMModificationRequest mdmModRequest =
-          new MDMModificationRequest()
-              .data(new MDMModificationRequestData().definition(items));
-
-      mdmApi.importData(definition,
-          entry, mdmModRequest,
-          branchUri);
-      MDMEntryApi entryApi = mdmApi.getApi(definition, entry);
-      if (entryApi.getDescriptor().getVectorCollection() != null
-          && !ObjectUtils
-              .isEmpty(entryApi.getDescriptor().getVectorCollection().getVectorCollectionName())) {
-        entryApi.updateAllIndices();
-      }
-    } catch (Exception e) {
-      log.error("Error with the following entry:" + definition + StringConstant.DOT + entry);
-      throw new BusinessLogicException(
-          MessageFormat.format(localeSettingApi.get("importEntriesError"), index), e);
+    } catch (IOException e) {
+      log.error(e.getMessage(), e);
+      throw new IllegalStateException(e);
+    }
+    MDMModificationRequest mdmModRequest =
+        new MDMModificationRequest()
+            .data(new MDMModificationRequestData().definition(items));
+    mdmApi.importData(definition,
+        entry, mdmModRequest,
+        branchUri);
+    MDMEntryApi entryApi = mdmApi.getApi(definition, entry);
+    if (entryApi.getDescriptor().getVectorCollection() != null
+        && !ObjectUtils
+            .isEmpty(entryApi.getDescriptor().getVectorCollection().getVectorCollectionName())) {
+      entryApi.updateAllIndices();
     }
   }
-
 
 }
