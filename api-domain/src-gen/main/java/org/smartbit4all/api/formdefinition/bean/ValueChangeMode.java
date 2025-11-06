c73e9ac046f0e1e11de850d4690a5b6838f9380f
Zsolt Lipcsei <zsolt.lipcsei@it4all.hu>
2025-11-06 14:36:39 +0100

    [#25942] update ValueChangeMode enum description

--- api-domain/src-gen/main/java/org/smartbit4all/api/formdefinition/bean/ValueChangeMode.java
+++ api-domain/src-gen/main/java/org/smartbit4all/api/formdefinition/bean/ValueChangeMode.java
@@ -25,7 +25,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 /**
- * This enum defines the value change handling strategy. Currently only one method is supported, IMMEDIATE_ACTION, which will trigger an executeUiAction when the value is changed. 
+ * This enum defines the value change handling strategy. IMMEDIATE_ACTION will trigger an executeUiAction when the value is changed. BLUR which will trigger an executeUiAction when the focus is losted from the html element. 
  */
 public enum ValueChangeMode {
   
