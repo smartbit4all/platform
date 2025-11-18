e9d5992769a39da088758afa20781dd62b415c3b
mnagyit4all <mate.nagy@it4all.hu>
2025-11-18 09:00:22 +0100

    [#26214] ObjectApiUtilities
    Add function that checks the size of a list

--- api/src/main/java/org/smartbit4all/core/object/ObjectUtilityApiImpl.java
+++ api/src/main/java/org/smartbit4all/core/object/ObjectUtilityApiImpl.java
@@ -146,4 +146,9 @@ public class ObjectUtilityApiImpl implements ObjectUtilityApi {
     return objectApi.asList(clazz, value);
   }
 
+  @Override
+  public Integer SizeList(List<?> list) {
+    return list.size();
+  }
+
 }
