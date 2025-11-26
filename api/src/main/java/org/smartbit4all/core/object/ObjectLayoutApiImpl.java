15fa9ddbf59acd083702bb57af83b02a7169afb9
Szabolcs Bazil Papp <szabolcs.papp@it4all.hu>
2025-11-26 10:56:31 +0100

    [#26430] add ability to draft a form display for an arbitrary object map

--- api/src/main/java/org/smartbit4all/core/object/ObjectLayoutApiImpl.java
+++ api/src/main/java/org/smartbit4all/core/object/ObjectLayoutApiImpl.java
@@ -1,15 +1,28 @@
 package org.smartbit4all.core.object;
 
+import static org.smartbit4all.core.object.ObjectLayoutBuilder.datePicker;
+import static org.smartbit4all.core.object.ObjectLayoutBuilder.datetimePicker;
+import static org.smartbit4all.core.object.ObjectLayoutBuilder.floatingPointNumberField;
+import static org.smartbit4all.core.object.ObjectLayoutBuilder.form;
+import static org.smartbit4all.core.object.ObjectLayoutBuilder.integralNumberField;
+import static org.smartbit4all.core.object.ObjectLayoutBuilder.textfield;
+import static org.smartbit4all.core.object.ObjectLayoutBuilder.toggle;
 import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smartbit4all.api.collection.CollectionApi;
+import org.smartbit4all.api.formdefinition.bean.SmartWidgetDefinition;
 import org.smartbit4all.api.invocation.ApiNotFoundException;
 import org.smartbit4all.api.invocation.InvocationApi;
 import org.smartbit4all.api.invocation.bean.InvocationParameter;
@@ -23,6 +36,7 @@ import org.smartbit4all.api.object.bean.ObjectLayoutDescriptor;
 import org.smartbit4all.api.session.SessionApi;
 import org.smartbit4all.api.setting.LocaleSettingApi;
 import org.smartbit4all.api.smartcomponentlayoutdefinition.bean.LayoutDefinitionDescriptor;
+import org.smartbit4all.api.smartcomponentlayoutdefinition.bean.LayoutDirection;
 import org.smartbit4all.api.smartcomponentlayoutdefinition.bean.SmartComponentLayoutDefinition;
 import org.smartbit4all.api.view.bean.ComponentConstraint;
 import org.smartbit4all.api.view.bean.UiActionConstraint;
@@ -210,6 +224,84 @@ public class ObjectLayoutApiImpl implements ObjectLayoutApi {
   @Override
   public ObjectDisplay getSketchDisplay(ObjectNode objectNode, ObjectLayoutDescriptor descriptor) {
     return new ObjectDisplay(descriptor.getLayouts(), new ArrayList<>(), new ArrayList<>());
+  }
+
+  @Override
+  public SmartComponentLayoutDefinition constructSimpleLayout(Map<String, Object> o) {
+    return constructSimpleLayout(o, LayoutDirection.HORIZONTAL);
+  }
+
+  @Override
+  public SmartComponentLayoutDefinition constructSimpleLayout(Map<String, Object> o,
+      LayoutDirection orientation) {
+    final var primitives = flatten(o).toList();
+    final var formWidgets = primitives.stream()
+        .map(it -> suggestWidget(String.join(".", it.keySegments), it.value))
+        .flatMap(Optional::stream)
+        .toArray(SmartWidgetDefinition[]::new);
+    return form(orientation, formWidgets);
+  }
+
+  private record ObjectMapValue(String[] keySegments, Object value) {
+
+    String[] append(String segment) {
+      String[] res = new String[keySegments.length + 1];
+      System.arraycopy(keySegments, 0, res, 0, keySegments.length);
+      res[res.length - 1] = segment;
+      return res;
+    }
+
+  }
+
+  private Stream<ObjectMapValue> flatten(Map<String, Object> o) {
+    return o.entrySet().stream()
+        .map(e -> new ObjectMapValue(new String[] {e.getKey()}, e.getValue()))
+        .flatMap(this::flatten);
+  }
+
+  private Stream<ObjectMapValue> flatten(ObjectMapValue v) {
+    final var o = v.value;
+    if (o instanceof Map<?, ?>) {
+      @SuppressWarnings({"unchecked"})
+      final Map<String, Object> m = (Map<String, Object>) o;
+      return m.entrySet().stream()
+          .map(e -> new ObjectMapValue(v.append(e.getKey()), e.getValue()))
+          .flatMap(this::flatten);
+    } else if (o == null || o instanceof Collection<?>) {
+      return Stream.empty();
+    }
+
+    return Stream.of(v);
+  }
+
+  private Optional<SmartWidgetDefinition> suggestWidget(String key, Object o) {
+    if (o instanceof String) {
+      return Optional.of(textfield(key, localeSettingApi.get(key)));
+    }
+
+    if (o instanceof Boolean) {
+      return Optional.of(toggle(key, localeSettingApi.get(key)));
+    }
+
+    if (o instanceof LocalDate) {
+      return Optional.of(datePicker(key, localeSettingApi.get(key)));
+    }
+
+    if (o instanceof LocalDateTime || o instanceof OffsetDateTime) {
+      return Optional.of(datetimePicker(key, localeSettingApi.get(key)));
+    }
+
+    if (Set.of(Short.class, Integer.class, Long.class, short.class, int.class,
+        long.class).stream().anyMatch(it -> it.isInstance(o))) {
+      return Optional.of(integralNumberField(key, localeSettingApi.get(key)));
+    }
+
+    if (Set.of(Float.class, Double.class, float.class, double.class).stream()
+        .anyMatch(it -> it.isInstance(o))) {
+      return Optional.of(floatingPointNumberField(key, localeSettingApi.get(key)));
+    }
+
+    return Optional.empty();
   }
 
 }
